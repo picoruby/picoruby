28ef0efa2ba487492de2bd8154310737a3d5c7cf
HASUMI Hitoshi <hasumikin@gmail.com>
2020-01-18 22:49:32 +0900

    initial commit

---
+++ Makefile
@@ -0,0 +1,9 @@
+all:
+	cd cli ; $(MAKE) all
+
+clean:
+	cd src ; $(MAKE) clean
+	cd cli ; $(MAKE) clean
+
+install:
+	cp cli/mmrbc /usr/local/bin/mmrbc
---
+++ cli/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+  // insert code here...
+  printf("argc : %d\n", argc);
+  printf("argv[0] : %s\n", argv[0]);
+  printf("argv[1] : %s\n", argv[1]);
+  printf("argv[2] : %s\n", argv[2]);
+  return 0;
+}
