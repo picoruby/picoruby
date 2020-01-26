574ba7879f835edc3126849e6f335a16a02b0ef1
HASUMI Hitoshi <hasumikin@gmail.com>
2020-01-26 13:59:06 +0900

    add version number

--- cli/main.c
+++ cli/main.c
@@ -1,11 +1,52 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "../src/version.h"
+
+typedef struct {
+  int verbose;
+} opt_t;
+
+int handle_opt(const char **argv, opt_t* opt)
+{
+  if (!strcmp(*argv, "-v") || !strcmp(*argv, "--version")) {
+    printf("mini mruby compiler %s\n", MMRBC_VERSION);
+    return 0;
+  } else if (!strcmp(*argv, "--verbose")) {
+    opt->verbose = 1;
+  }
+  return -1;
+}
 
 int main(int argc, const char * argv[])
 {
-  // insert code here...
-  printf("argc : %d\n", argc);
-  printf("argv[0] : %s\n", argv[0]);
-  printf("argv[1] : %s\n", argv[1]);
-  printf("argv[2] : %s\n", argv[2]);
+  opt_t opt;
+  opt.verbose = 0;
+  int c;
+  FILE *fp;
+
+  if(!*++argv) {
+    printf( "mmrbc: no program file given\n" );
+    return 1;
+  }
+
+  if(*argv[0] == '-') {
+    int res = handle_opt(argv, &opt);
+    if (res >= 0) {
+      return res;
+    } else {
+    }
+  }
+
+  if( (fp = fopen( *argv, "r" ) ) == NULL ) {
+    fprintf( stderr, "mmrbc: cannot open program file. (%s)\n", *argv );
+    return 1;
+  } else {
+    while( ( c = getc(fp) ) != EOF ) {
+      putchar(c);
+    }
+    putchar('\n');
+    fclose( fp );
+  }
   return 0;
 }
---
+++ src/version.h
@@ -0,0 +1,6 @@
+#ifndef MMRBC_VERSION_H
+#define MMRBC_VERSION_H
+
+#define MMRBC_VERSION "0.0.1"
+
+#endif /* MMRBC_VERSION_H */
