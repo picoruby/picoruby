aceee157da98094b1d6ecebc18c28322fe821215
HASUMI Hitoshi <hasumikin@gmail.com>
2020-03-22 16:50:14 +0900

    eliminate flattenCode object

--- src/generator.c
+++ src/generator.c
@@ -117,16 +117,14 @@ void codegen(Scope *scope, Node *tree)
   }
 }
 
-uint8_t *flattenCode(CodeSnippet *code_snippet, int codeSize)
+void memcpyFlattenCode(uint8_t *body, CodeSnippet *code_snippet, int size)
 {
-  uint8_t *result = mmrbc_alloc(codeSize);
-  int index = 0;
+  int pos = 0;
   while (code_snippet != NULL) {
-    memcpy(&result[index], code_snippet->value, code_snippet->size);
-    index += code_snippet->size;
+    memcpy(&body[pos], code_snippet->value, code_snippet->size);
+    pos += code_snippet->size;
     code_snippet = code_snippet->next;
   }
-  return result;
 }
 
 MrbCode *Generator_generate(Node *root)
@@ -142,7 +140,7 @@ MrbCode *Generator_generate(Node *root)
   vmCode[12] = (codeSize >> 8) & 0xff;
   vmCode[13] = codeSize & 0xff;
   memcpy(&vmCode[14], "MATZ0000", 8);
-  memcpy(&vmCode[22], flattenCode(scope->code_snippet, irepSize), irepSize);
+  memcpyFlattenCode(&vmCode[22], scope->code_snippet, irepSize);
   memcpy(&vmCode[22 + irepSize], "END\0\0\0\0", 7);
   vmCode[22 + irepSize + 7] = 0x08;
   uint16_t crc = calc_crc_16_ccitt(&vmCode[10], codeSize - 10, 0);
