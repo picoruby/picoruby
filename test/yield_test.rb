191ca0f33586fe13b91274aeb691e6acd7a3347d
HASUMI Hitoshi <hasumikin@gmail.com>
2021-07-29 18:00:39 +0900

    improve test

--- test/yield_test.rb
+++ test/yield_test.rb
@@ -1,11 +1,11 @@
 class YieldTest < PicoRubyTest
   desc "do block"
-  assert_equal(<<~'RUBY', "Hello Ruby")
-    def my_method(v)
-      yield v
+  assert_equal(<<~'RUBY', "Hello PicoRuby")
+    def my_method(m, n)
+      yield m, n
     end
-    my_method('Ruby') do |v|
-      puts \"Hello #{v}\"
+    my_method('Ruby', 'Pico') do |v, x|
+      puts \"Hello #{x}#{v}\"
     end
   RUBY
 end
