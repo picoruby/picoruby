11cb1177c554699a1463d23f3a3f9c205f793108
HASUMI Hitoshi <hasumikin@gmail.com>
2021-08-03 14:21:56 +0900

    Fix Makefile

--- cli/picorbc.c
+++ cli/picorbc.c
@@ -80,17 +80,12 @@ int handle_opt(int argc, char * const *argv, char *out, char *b_symbol)
   return 0;
 }
 
-const char C_FORMAT_LINES[10][28] = {
+const char C_FORMAT_LINES[5][22] = {
   "#include <stdint.h>",
   "#ifdef __cplusplus",
   "extern const uint8_t ",
   "#endif",
-  "const uint8_t",
-  "#if defined __GNUC__",
-  "__attribute__((aligned(4)))",
-  "#elif defined _MSC_VER",
-  "__declspec(align(4))",
-  "#endif"
+  "const uint8_t ",
 };
 
 int output(Scope *scope, char *in, char *out, char *b_symbol)
@@ -117,13 +112,13 @@ int output(Scope *scope, char *in, char *out, char *b_symbol)
       fwrite(scope->vm_code, scope->vm_code_size, 1, fp);
     } else {
       int i;
-      for (i=0; i < 10; i++) {
+      for (i=0; i < 5; i++) {
         fwrite(C_FORMAT_LINES[i], strlen(C_FORMAT_LINES[i]), 1, fp);
         if (i == 2) {
           fwrite(b_symbol, strlen(b_symbol), 1, fp);
           fwrite("[];", 3, 1, fp);
         }
-        fwrite("\n", 1, 1, fp);
+        if (i < 4) fwrite("\n", 1, 1, fp);
       }
       fwrite(b_symbol, strlen(b_symbol), 1, fp);
       fwrite("[] = {", 6, 1, fp);
@@ -140,6 +135,8 @@ int output(Scope *scope, char *in, char *out, char *b_symbol)
   return 0;
 }
 
+#define BUFSIZE 1024
+
 int main(int argc, char * const *argv)
 {
   char out[255];
@@ -154,7 +151,39 @@ int main(int argc, char * const *argv)
     return 1;
   }
 
-  char *in = argv[optind];
+  char *in;
+  { /* Concatenate files or single file */
+    FILE *infp, *outfp;
+    int i;
+    char buf[BUFSIZE];
+    int readsize;
+    if (!argv[optind + 1]) { /* sigle input file */
+      in = argv[optind];
+    } else {                 /* multiple input files */
+      in = tmpnam(NULL);
+      if (!in) {
+        FATALP("Could't get tempfile name");
+        return -1;
+      }
+      outfp = fopen(in, "wb");
+      if (!outfp) {
+        FATALP("Could't open tempfile");
+        return -1;
+      }
+      i = optind;
+      while (argv[i]) {
+        infp = fopen(argv[i], "rb");
+        if (!infp) {
+          FATALP("Could't open file: %s", argv[i]);
+          return -1;
+        }
+        while ((readsize = fread(buf, 1, BUFSIZE, infp)) != 0)fwrite(buf, 1, readsize, outfp);
+        fclose(infp);
+        i++;
+      }
+      fclose(outfp);
+    }
+  }
 
   StreamInterface *si = StreamInterface_new(in, STREAM_TYPE_FILE);
   if (si == NULL) return 1;
