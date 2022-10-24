8a6b3a92ad891ad304303b95518941040bed43c5
HASUMI Hitoshi <hasumikin@gmail.com>
2022-10-24 13:32:02 +0900

    Fix hal

--- build_config/prsh-lib_arm-none-eabi.rb
+++ build_config/prsh-lib_arm-none-eabi.rb
@@ -1,5 +1,12 @@
 MRuby::CrossBuild.new("prsh_arm-none-eabi") do |conf|
 
+  ###############################################################
+  # You need following tools:
+  #   arm-none-eabi       | to make libmruby.a
+  #   arm-linux-gnueabihf | to make lemon and ptr_size_generator
+  #   qemu-arm-static     | to run ptr_size_generator
+  ###############################################################
+
   conf.toolchain
   conf.cc.flags << "-static"
   conf.linker.flags << "-static"
@@ -9,6 +16,10 @@ MRuby::CrossBuild.new("prsh_arm-none-eabi") do |conf|
   conf.cc.alt_command = "arm-linux-gnueabihf-gcc"
   conf.archiver.alt_command = "arm-linux-gnueabihf-ar"
   ENV['QEMU'] = "qemu-arm-static"
+
+  # hal configuration
+  conf.cc.defines << "MRBC_TICK_UNIT=1"
+  conf.cc.defines << "MRBC_TIMESLICE_TICK_COUNT=10"
 
   conf.picoruby
 
--- include/hal_no_impl/hal.c
+++ include/hal_no_impl/hal.c
@@ -1,1 +1,7 @@
-/* implementation */
+/*
+ *
+ * This hal.c has no implementation in order to make
+ * libmruby.a that doesn't depend on each platform.
+ *
+ * */
+
