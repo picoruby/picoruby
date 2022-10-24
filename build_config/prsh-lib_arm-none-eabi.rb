24c16d885b6045471becebfb40643f10bb5b77de
HASUMI Hitoshi <hasumikin@gmail.com>
2022-10-24 21:36:14 +0900

    Fix hal

--- build_config/prsh-lib_arm-none-eabi.rb
+++ build_config/prsh-lib_arm-none-eabi.rb
@@ -17,15 +17,8 @@ MRuby::CrossBuild.new("prsh_arm-none-eabi") do |conf|
   conf.archiver.alt_command = "arm-linux-gnueabihf-ar"
   ENV['QEMU'] = "qemu-arm-static"
 
-  # hal configuration
-  conf.cc.defines << "MRBC_TICK_UNIT=1"
-  conf.cc.defines << "MRBC_TIMESLICE_TICK_COUNT=10"
-
+  conf.mrubyc_hal_arm
   conf.picoruby
-
-  conf.gem github: 'hasumikin/mruby-pico-compiler', branch: 'master'
-
-  conf.gem core: 'picoruby-mrubyc'
 
   conf.gem core: "picoruby-filesystem-fat"
   conf.gem core: "picoruby-vim"
