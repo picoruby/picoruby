866d5a93bfb73d0b3c6691ed9dea630b38d6d35c
HASUMI Hitoshi <hasumikin@gmail.com>
2022-11-01 13:23:45 +0900

    Fix exit on POSIX

--- mrbgems/picoruby-bin-prsh/tools/prsh/prsh.c
+++ mrbgems/picoruby-bin-prsh/tools/prsh/prsh.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 
 #include <picorbc.h>
 #include <picogem_init.c>
@@ -12,10 +13,18 @@
 int loglevel = LOGLEVEL_FATAL; /* must be LOGLEVEL_FATAL so that irb works */
 static uint8_t heap_pool[HEAP_SIZE];
 
+static void
+c_exit(mrb_vm *vm, mrb_value *v, int argc)
+{
+  pid_t pid = getpid();
+  kill(pid, SIGINT);
+}
+
 int
 main(void)
 {
   mrbc_init(heap_pool, HEAP_SIZE);
+  mrbc_define_method(0, mrbc_class_object, "exit", c_exit);
   mrbc_require_init();
   mrbc_create_task(prsh, 0);
   mrbc_run();
