619a5b9a6fae4a269f874967e21deca75603d888
HASUMI Hitoshi <hasumikin@gmail.com>
2022-11-03 23:17:28 +0900

    For workaround of using RAM instead of ROM

--- mrbgems/picoruby-filesystem-fat/src/hal/ram_disk.h
+++ mrbgems/picoruby-filesystem-fat/src/hal/ram_disk.h
@@ -8,6 +8,16 @@
 extern "C" {
 #endif
 
+/*
+ * Volume size = SECTOR_SIZE * SECTOR_COUNT
+ *  512 * 192 == 96 KiB
+ *  Seems this is the minimum volume size of FatFS
+ */
+#define SECTOR_SIZE   512
+#define SECTOR_COUNT  192
+
+extern BYTE *ram_disk;
+
 int RAM_disk_erase(void);
 int RAM_disk_initialize(void);
 int RAM_disk_status(void);
