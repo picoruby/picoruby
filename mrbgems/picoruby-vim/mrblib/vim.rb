7a103bd69d83f0cc4257968272264858c7caf2ae
HASUMI Hitoshi <hasumikin@gmail.com>
2022-11-05 17:14:34 +0900

    Vim can insert and save file!!!

--- mrbgems/picoruby-vim/mrblib/vim.rb
+++ mrbgems/picoruby-vim/mrblib/vim.rb
@@ -1,12 +1,12 @@
 #! /usr/bin/env ruby
 
 case RUBY_ENGINE
-when "ruby"
+when "ruby", "jruby"
   require_relative "../../picoruby-terminal/mrblib/terminal"
 when "mruby/c"
   require "terminal"
 else
-  raise RuntimeError.new("Unknown RUBY_ENGINE")
+  raise "Unknown RUBY_ENGINE: #{RUBY_ENGINE}"
 end
 
 class Vim
@@ -14,27 +14,31 @@ class Vim
     @filepath = File.expand_path filepath, Dir.getwd if filepath
     @mode = :normal
     @terminal = Terminal::Editor.new
+    @terminal.quit_by_ctrl_c = false
     @terminal.footer_height = 2
     @command_buffer = Terminal::Buffer.new
+    @message = nil
     if @filepath
       unless @terminal.load_file_into_buffer(@filepath)
-        @command_buffer.lines[0] == "No found"
+        @command_buffer.lines[0] = "\"#{filepath}\" [New]"
       end
     end
     @terminal.refresh_footer do |terminal|
-      #     foreground  background
-      print "\e[37;1m" ,"\e[48;5;239m"
+      print "\e[37;1m\e[48;5;239m " # foreground & background
       if @filepath
-        print " ", @filepath[0, terminal.width - 1].ljust(terminal.width - 1)
+        print @filepath[0, terminal.width - 1].ljust(terminal.width - 1)
       else
-        print " ", "[No Name]".ljust(terminal.width - 1)
+        print "[No Name]".ljust(terminal.width - 1)
       end
-      print "\e[m" # reset color
-      print "\e[1E"
-      print @command_buffer.lines[0]
+      print "\e[m\e[1E"
+      if @message
+        print "\e[31m", @message, "\e[m"
+        @message = nil
+      else
+        print "\e[m", @command_buffer.lines[0]
+      end
       if @mode == :command
-        print "\e[#{terminal.width};1H"
-        print "\e[#{@command_buffer.lines[0].size}C"
+        print "\e[#{terminal.width};1H\e[#{@command_buffer.lines[0].size}C"
       end
     end
     @terminal.refresh_cursor do |terminal|
@@ -59,8 +63,10 @@ class Vim
     @terminal.start do |terminal, buffer, c|
       case @mode
       when :normal
-        if c < 108
+        if c < 112
           case c
+          when  3 # Ctrl-C
+            @command_buffer = "Type  :q  and press <Enter> to exit"
           when  13 # LF
           when  27 # ESC
             case IO.get_nonblock(2)
@@ -79,7 +85,30 @@ class Vim
           when  59 # : command
             @mode = :command
             @command_buffer.put ':'
-          when  65 # A
+          when 65, 73, 97, 105, 111 # A, I, a, i, o -> insert
+            case c
+            when 65 # A
+              buffer.tail
+            when 73 # I
+              buffer.head
+              while true
+                break unless buffer.current_char == " "
+                buffer.put :RIGHT
+              end
+            when 97 # a
+              buffer.put :RIGHT
+            when 111 # o
+              buffer.tail
+              buffer.put :ENTER
+            end
+            @mode = :insert
+            @command_buffer.lines[0] = "Insert"
+          when 106 # j down
+            buffer.put :DOWN
+          when 107 # k up
+            buffer.put :UP
+          when 108 # l right
+            buffer.put :RIGHT
           when  86 # V
           when  98 # b begin
           when 100 # d
@@ -87,20 +116,9 @@ class Vim
           when 103 # g
           when 104 # h left
             buffer.put :LEFT
-          when 97, 105 # a, i insert
-            right if c == 97 # a
-            @mode = :insert
-            @command_buffer.lines[0] = "Insert"
-          when 106 # j down
-            buffer.put :DOWN
-          when 107 # k up
-            buffer.put :UP
           end
         else
           case c
-          when 108 # l right
-            buffer.put :RIGHT
-          when 111 # o replace
           when 112 # p paste
           when 114 # r replace
           when 117 # u undo
@@ -114,6 +132,15 @@ class Vim
         end
       when :command
         case c
+        when 10, 13
+          case res = exec_command(buffer)
+          when :quit
+            raise "__quit()"
+          else
+            @message = res
+            @command_buffer.clear
+            @mode = :normal
+          end
         when 27 # ESC
           case IO.get_nonblock(2)
           when "[C" # right
@@ -147,6 +174,7 @@ class Vim
           when nil
             @command_buffer.clear
             @mode = :normal
+            buffer.put :LEFT if 0 < buffer.cursor_x
           end
         when 8, 127 # 127 on UNIX
           buffer.put :BSPACE
@@ -161,6 +189,51 @@ class Vim
       when :visual_line
       when :visual_block
       end
+    end
+  end
+
+  def exec_command(buffer)
+    params = @command_buffer.lines[0].split(" ")
+    case params.count
+    when 0
+      # do nothing
+    when 1
+      case params[0]
+      when ":q"
+        if buffer.changed
+          "No write since last change"
+        else
+          :quit
+        end
+      when ":q!"
+        :quit
+      when ":w"
+        save_file
+      when ":wq"
+        save_file
+        :quit
+      else
+        "Not an editor command: #{@command_buffer.lines[0]}"
+      end
+    when 2
+      case params[0]
+      when ":w"
+        @filepath = File.expand_path params[1], Dir.getwd
+        save_file
+      else
+        "Not an editor command: #{@command_buffer.lines[0]}"
+      end
+    else
+      "Not an editor command: #{@command_buffer.lines[0]}"
+    end
+  end
+
+  def save_file
+    return "No file name" unless @filepath
+    if @terminal.save_file_from_buffer(@filepath)
+      "File saved: #{@filepath}"
+    else
+      "Failed to save file: #{e.message}"
     end
   end
 end
