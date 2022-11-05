6228fc10ddeee879b4419454db3c63d606f230d4
HASUMI Hitoshi <hasumikin@gmail.com>
2022-11-05 20:48:06 +0900

    memory_statistics

--- mrbgems/picoruby-shell/src/shell.c
+++ mrbgems/picoruby-shell/src/shell.c
@@ -1,5 +1,41 @@
+#include <mrubyc.h>
+
+#ifndef MRBC_ALLOC_LIBC
+static void
+c_memory_statistics(struct VM *vm, mrbc_value v[], int argc)
+{
+  struct MRBC_ALLOC_STATISTICS mem;
+  mrbc_alloc_statistics(&mem);
+  mrbc_value ret = mrbc_hash_new(vm, 4);
+  mrbc_hash_set(
+    &ret,
+    &mrbc_symbol_value(mrbc_str_to_symid("total")),
+    &mrbc_integer_value(mem.total)
+  );
+  mrbc_hash_set(
+    &ret,
+    &mrbc_symbol_value(mrbc_str_to_symid("used")),
+    &mrbc_integer_value(mem.used)
+  );
+  mrbc_hash_set(
+    &ret,
+    &mrbc_symbol_value(mrbc_str_to_symid("free")),
+    &mrbc_integer_value(mem.free)
+  );
+  mrbc_hash_set(
+    &ret,
+    &mrbc_symbol_value(mrbc_str_to_symid("frag")),
+    &mrbc_integer_value(mem.fragmentation)
+  );
+  SET_RETURN(ret);
+}
+#endif /* MRBC_ALLOC_LIBC */
+
 void
 mrbc_shell_init(void)
 {
+#ifndef MRBC_ALLOC_LIBC
+  mrbc_define_method(0, mrbc_class_object, "memory_statistics", c_memory_statistics);
+#endif /* MRBC_ALLOC_LIBC */
 }
 
