e4e5ff9d57a520aabf4bc112290de39c1288b6be
HASUMI Hitoshi <hasumikin@gmail.com>
2022-11-06 13:20:21 +0900

    [WIP] executables

--- build_config/prsh-bin.rb
+++ build_config/prsh-bin.rb
@@ -6,6 +6,12 @@ MRuby::Build.new do |conf|
 
   conf.gembox "default"
 
+  conf.cc.flags.delete "-O3"
+  conf.cc.flags << "-O0"
+  conf.cc.flags << "-g"
+  conf.cc.flags << "-fno-inline"
+
+  conf.cc.defines << "MRBC_INT_64"
   conf.cc.defines << "MRBC_USE_HAL_POSIX"
   conf.cc.defines << "MRBC_ALLOC_LIBC"
   conf.cc.defines << "REGEX_USE_ALLOC_LIBC"
