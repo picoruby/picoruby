b3db0776e4b66b69a3d005a3a011967f16623d4c
HASUMI Hitoshi <hasumikin@gmail.com>
2022-11-07 14:57:56 +0900

    ls command

--- mrbgems/picoruby-shell/shell_executables/ls.rb
+++ mrbgems/picoruby-shell/shell_executables/ls.rb
@@ -1,5 +1,29 @@
-Dir.open("./") do |dir|
+arg = ARGV[0]
+
+dir, file = if arg.nil?
+  ["./", nil]
+elsif arg == "/"
+  ["/", nil]
+elsif arg.end_with?("/")
+  [arg, nil]
+else
+  elements = arg.split("/")
+  file = elements.pop
+  elements[0] = "/" if elements[0].to_s == ""
+  [elements.join("/") , file]
+end
+
+if dir != "/" && Dir.exist?("#{dir}/#{file}")
+  dir = "#{dir}/#{file}"
+  file = nil
+end
+
+Dir.open(dir) do |dir|
+  # TODO: handle file
+  feed = false
   while entry = dir.read
-    puts entry
+    print entry, "  "
+    feed = true
   end
+  print "\n" if feed
 end
