ebb666d2e7a71dcb19238af76c093cd684ca9b07
HASUMI Hitoshi <hasumikin@gmail.com>
2022-11-07 18:03:57 +0900

    mrbc command

--- mrbgems/picoruby-shell/src/shell.c
+++ mrbgems/picoruby-shell/src/shell.c
@@ -1,3 +1,4 @@
+#include <picorbc.h>
 #include <mrubyc.h>
 
 static void
@@ -39,9 +40,26 @@ c_memory_statistics(struct VM *vm, mrbc_value v[], int argc)
   SET_RETURN(ret);
 }
 
+static void
+c_mrbc(struct VM *vm, mrbc_value v[], int argc)
+{
+
+  StreamInterface *si = StreamInterface_new(NULL, GET_STRING_ARG(1), STREAM_TYPE_MEMORY);
+  ParserState *p = Compiler_parseInitState(0, si->node_box_size);
+  if (Compiler_compile(p, si, NULL)) {
+    mrbc_value mrb = mrbc_string_new(vm, p->scope->vm_code, p->scope->vm_code_size);
+    SET_RETURN(mrb);
+  } else {
+    mrbc_raise(vm, MRBC_CLASS(RuntimeError), "Compile failed");
+  }
+  StreamInterface_free(si);
+  Compiler_parserStateFree(p);
+}
+
 void
 mrbc_shell_init(void)
 {
   mrbc_define_method(0, mrbc_class_object, "memory_statistics", c_memory_statistics);
+  mrbc_define_method(0, mrbc_class_object, "mrbc", c_mrbc);
 }
 
