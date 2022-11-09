de81136936ae836f19b64652ae8211f88f52ee8b
HASUMI Hitoshi <hasumikin@gmail.com>
2022-11-09 11:32:06 +0900

    Not to wait if executable

--- mrbgems/picoruby-sandbox/mrblib/sandbox.rb
+++ mrbgems/picoruby-sandbox/mrblib/sandbox.rb
@@ -2,13 +2,13 @@ class Sandbox
 
   TIMEOUT = 10_000 # 10 sec
 
-  def wait
+  def wait(timeout = TIMEOUT)
     n = 0
     # state 0: TASKSTATE_DORMANT == finished
     while self.state != 0 do
       sleep_ms 50
       n += 50
-      if TIMEOUT < n
+      if timeout && timeout < n
         puts "Error: Timeout (sandbox.state: #{self.state})"
         return false
       end
