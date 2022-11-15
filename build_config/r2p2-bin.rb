bc754cb526fd946e6e4323266f99117f75fe8052
HASUMI Hitoshi <hasumikin@gmail.com>
2022-11-15 17:31:13 +0900

    define TYPO

--- build_config/r2p2-bin.rb
+++ build_config/r2p2-bin.rb
@@ -11,7 +11,7 @@ MRuby::Build.new do |conf|
   conf.cc.flags << "-g"
   conf.cc.flags << "-fno-inline"
 
-  conf.cc.defines << "MRBC_INT_64"
+  conf.cc.defines << "MRBC_INT64"
   conf.cc.defines << "MRBC_USE_HAL_POSIX"
   conf.cc.defines << "MRBC_ALLOC_LIBC"
   conf.cc.defines << "REGEX_USE_ALLOC_LIBC"
