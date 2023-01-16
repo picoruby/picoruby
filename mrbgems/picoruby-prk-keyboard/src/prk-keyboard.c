bc40f01816432f17ff6a00d1229b4e5aebcdb79a
HASUMI Hitoshi <hasumikin@gmail.com>
2023-01-16 15:05:01 +0900

    remove unused code

--- mrbgems/picoruby-prk-keyboard/src/prk-keyboard.c
+++ mrbgems/picoruby-prk-keyboard/src/prk-keyboard.c
@@ -1,41 +1,12 @@
 #include <mrubyc.h>
 #include "../include/prk-keyboard.h"
 
-#ifndef PRK_NO_MSC
-mrbc_tcb *tcb_keymap;
-
-static uint32_t buffer = 0;
-static int      buffer_index = 0;
-
-static void
-c_Keyboard_resume_keymap(mrbc_vm *vm, mrbc_value *v, int argc)
-{
-  mrbc_resume_task(tcb_keymap);
-}
-
-static void
-c_Keyboard_suspend_keymap(mrbc_vm *vm, mrbc_value *v, int argc)
-{
-  mrbc_suspend_task(tcb_keymap);
-}
-
-/*
- * TODO: The current implemantation is in prk_firmware/src/main.c
- *       Move that to this gem along with taking advantage of File and Dir class
- */
-mrbc_tcb* create_keymap_task(mrbc_tcb *tcb);
-
-//static void
-//c_Keyboard_reload_keymap(mrbc_vm *vm, mrbc_value *v, int argc)
-//{
-//  tcb_keymap = create_keymap_task(tcb_keymap);
-//}
-#endif /* PRK_NO_MSC */
-
-
 /*
  * Software UART
  */
+
+static uint32_t buffer = 0;
+static int      buffer_index = 0;
 
 static void
 c_uart_anchor_init(mrb_vm *vm, mrb_value *v, int argc)
@@ -78,12 +49,6 @@ void
 mrbc_prk_keyboard_init(void)
 {
   mrbc_class *mrbc_class_Keyboard = mrbc_define_class(0, "Keyboard", mrbc_class_object);
-
-#ifndef PRK_NO_MSC
-//  mrbc_define_method(0, mrbc_class_Keyboard, "reload_keymap",  c_Keyboard_reload_keymap);
-  mrbc_define_method(0, mrbc_class_Keyboard, "suspend_keymap", c_Keyboard_suspend_keymap);
-  mrbc_define_method(0, mrbc_class_Keyboard, "resume_keymap",  c_Keyboard_resume_keymap);
-#endif /* PRK_NO_MSC */
 
   mrbc_define_method(0, mrbc_class_Keyboard, "uart_anchor_init",   c_uart_anchor_init);
   mrbc_define_method(0, mrbc_class_Keyboard, "uart_partner_init",  c_uart_partner_init);
