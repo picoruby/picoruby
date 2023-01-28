21bc3cfb66a17a431d9f5034bb1630b817f8cc83
HASUMI Hitoshi <hasumikin@gmail.com>
2023-01-28 13:12:35 +0900

    wip GPIO

--- mrbgems/picoruby-gpio/mrblib/gpio.rb
+++ mrbgems/picoruby-gpio/mrblib/gpio.rb
@@ -1,6 +1,7 @@
 class GPIO
   # This is a mimic of pico_error_codes
   # from pico-sdk/src/common/pico_base/include/pico/error.h
+  ERROR_NONE = 0
   ERROR_TIMEOUT = -1
   ERROR_GENERIC = -2
   ERROR_NO_DATA = -3
@@ -10,12 +11,79 @@ class GPIO
 
   def handle_error(code, name = "")
     case code
+    when ERROR_NONE
+      return 0
     when ERROR_TIMEOUT
       raise(RuntimeError.new("Timeout error in #{name}"))
     else
       raise(RuntimeError.new("Unknown error in #{name}. code: #{code}"))
     end
-    code
+  end
+
+  def initialize(pin, *params)
+    @pin = pin
+    GPIO._init(pin)
+    mode = disjunction(params)
+    set_dir(mode)
+    unless @dir
+      raise ArgumentError.new("You must specify one of IN, OUT and HIGH_Z")
+    end
+    set_pull(mode, true)
+    set_open_drain(mode, true)
+  end
+
+  def setmode(*params)
+    mode = disjunction(params)
+    set_dir(mode) if 0 < (mode & (IN|OUT|HIGH_Z))
+    set_pull(mode)
+    set_open_drain(mode)
+  end
+
+  # private
+
+  def disjunction(params)
+    mode = 0
+    params.each { |param| mode |= param }
+    mode
+  end
+
+  def set_dir(mode)
+    mode_dir = ((mode & IN) + ((mode & OUT)>>1) + ((mode & HIGH_Z)>>2))
+    case mode_dir
+    when 0
+      # do nothing
+    when 1
+      @dir = mode & (IN|OUT|HIGH_Z)
+      GPIO._set_dir(@pin, @dir.to_i)
+      return 0
+    else
+      raise ArgumentError.new("IN, OUT and HIGH_Z are exclusive")
+    end
+    0
+  end
+
+  def set_pull(mode, initialize = false)
+    if initialize || 0 < (mode & (PULL_UP|PULL_DOWN))
+      @pull = case mode & (PULL_UP|PULL_DOWN)
+      when 0
+        nil
+      when PULL_UP, PULL_DOWN
+        GPIO._set_pull(@pin, @pull.to_i)
+        @pull
+      else
+        raise ArgumentError.new("PULL_UP and PULL_DOWN are exclusive")
+      end
+    end
+    0
+  end
+
+  def set_open_drain(mode, initialize = false)
+    if initialize || 0 < (mode & OPEN_DRAIN)
+      @open_drain = (0 < (mode & 0b1000000))
+      GPIO._set_open_drain(@pin) if @open_drain
+      @open_drain
+    end
+    0
   end
 end
 
---
+++ mrbgems/picoruby-gpio/src/gpio.c
@@ -0,0 +1,179 @@
+#include <stdbool.h>
+#include <mrubyc.h>
+#include "../include/gpio.h"
+
+#define GETIV(str)       mrbc_instance_getiv(&v[0], mrbc_str_to_symid(#str))
+#define READ(pin)        GPIO_read(pin_num(vm, pin))
+#define WRITE(pin, val)  GPIO_write(pin_num(vm, pin), val)
+
+static int
+pin_num(mrbc_vm *vm, mrbc_value pin)
+{
+  switch (mrbc_type(pin)) {
+    case MRBC_TT_INTEGER: {
+      return pin.i;
+    }
+    case MRBC_TT_STRING: {
+      return GPIO_pin_num_from_char(pin.string->data);
+    }
+    default:
+      mrbc_raise(vm, MRBC_CLASS(ArgumentError), "Wrong GPIO pin value");
+      return -1;
+  }
+}
+
+/*
+ * GPIO._init(pin)
+ */
+static void
+c__init(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  GPIO_init(pin_num(vm, v[1]));
+}
+
+/*
+ * GPIO._set_dir(pin, dir)
+ */
+static void
+c__set_dir(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  GPIO_set_dir(pin_num(vm, v[1]), GET_INT_ARG(2));
+  SET_INT_RETURN(0);
+}
+
+/*
+ * GPIO._set_open_drain(pin)
+ */
+static void
+c__set_open_drain(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  GPIO_set_open_drain(pin_num(vm, v[1]));
+  SET_INT_RETURN(0);
+}
+
+/*
+ * GPIO._set_pull(pin, pull)
+ */
+static void
+c__set_pull(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  GPIO_set_pull(pin_num(vm, v[1]), GET_INT_ARG(2));
+  SET_INT_RETURN(0);
+}
+
+/*
+ * GPIO.high_at?(pin)
+ */
+static void
+c_high_at_q(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  if (READ(v[1]) == 0) {
+    SET_FALSE_RETURN();
+  } else {
+    SET_TRUE_RETURN();
+  }
+}
+
+/*
+ * GPIO.high?(pin)
+ */
+static void
+c_high_q(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  if (READ(GETIV("pin")) == 0) {
+    SET_FALSE_RETURN();
+  } else {
+    SET_TRUE_RETURN();
+  }
+}
+
+/*
+ * GPIO.low_at?(pin)
+ */
+static void
+c_low_at_q(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  if (READ(v[1]) == 0) {
+    SET_TRUE_RETURN();
+  } else {
+    SET_FALSE_RETURN();
+  }
+}
+
+/*
+ * GPIO.low?(pin)
+ */
+static void
+c_low_q(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  if (READ(GETIV("pin")) == 0) {
+    SET_TRUE_RETURN();
+  } else {
+    SET_FALSE_RETURN();
+  }
+}
+
+/*
+ * GPIO.read_at(pin)
+ */
+static void
+c_read_at(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  SET_INT_RETURN(READ(v[1]));
+}
+
+/*
+ * GPIO.read(pin)
+ */
+static void
+c_read(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  SET_INT_RETURN(READ(GETIV("pin")));
+}
+
+/*
+ * GPIO.write_at(pin, val)
+ */
+static void
+c_write_at(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  WRITE(v[1], GET_INT_ARG(2));
+  SET_INT_RETURN(0);
+}
+
+/*
+ * GPIO#write(val)
+ */
+static void
+c_write(mrbc_vm *vm, mrb_value *v, int argc)
+{
+  WRITE(GETIV("pin"), GET_INT_ARG(1));
+  SET_INT_RETURN(0);
+}
+
+void
+mrbc_gpio_init(void)
+{
+  mrbc_class *mrbc_class_GPIO = mrbc_define_class(0, "GPIO", mrbc_class_object);
+
+  mrbc_set_const(mrbc_str_to_symid("IN"), &mrbc_integer_value(IN));
+  mrbc_set_const(mrbc_str_to_symid("OUT"), &mrbc_integer_value(OUT));
+  mrbc_set_const(mrbc_str_to_symid("HIGH_Z"), &mrbc_integer_value(HIGH_Z));
+  mrbc_set_const(mrbc_str_to_symid("PULL_UP"), &mrbc_integer_value(PULL_UP));
+  mrbc_set_const(mrbc_str_to_symid("PULL_DOWN"), &mrbc_integer_value(PULL_DOWN));
+  mrbc_set_const(mrbc_str_to_symid("OPEN_DRAIN"), &mrbc_integer_value(OPEN_DRAIN));
+
+  mrbc_define_method(0, mrbc_class_GPIO, "_init", c__init);
+  mrbc_define_method(0, mrbc_class_GPIO, "_set_dir", c__set_dir);
+  mrbc_define_method(0, mrbc_class_GPIO, "_set_pull", c__set_pull);
+  mrbc_define_method(0, mrbc_class_GPIO, "_set_open_drain", c__set_open_drain);
+  mrbc_define_method(0, mrbc_class_GPIO, "high_at?", c_high_at_q);
+  mrbc_define_method(0, mrbc_class_GPIO, "high?", c_high_q);
+  mrbc_define_method(0, mrbc_class_GPIO, "low_at?", c_low_at_q);
+  mrbc_define_method(0, mrbc_class_GPIO, "low?", c_low_q);
+  mrbc_define_method(0, mrbc_class_GPIO, "read_at", c_read_at);
+  mrbc_define_method(0, mrbc_class_GPIO, "read", c_read);
+  mrbc_define_method(0, mrbc_class_GPIO, "write_at", c_write_at);
+  mrbc_define_method(0, mrbc_class_GPIO, "write", c_write);
+}
+
