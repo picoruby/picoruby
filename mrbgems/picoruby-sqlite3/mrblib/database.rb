7f00fbe2527ffa330475d682648791b0d0548f7b
HASUMI Hitoshi <hasumikin@gmail.com>
2023-04-07 10:49:24 +0900

    Fix RBS

--- mrbgems/picoruby-sqlite3/mrblib/database.rb
+++ mrbgems/picoruby-sqlite3/mrblib/database.rb
@@ -2,14 +2,18 @@ require "time"
 
 class SQLite3
   class Database
-    def execute(sql, &block)
-      rows = _execute(sql)
-      if block
+    def execute(sql, *params, &block)
+      rows = _execute(sql, params)
+      return rows unless block
+      case rows
+      when Array
         rows.each do |row|
           block.call(row)
         end
-      else
-        rows
+      when Hash
+        rows.each do |key, value|
+          block.call(key, value)
+        end
       end
     end
   end
