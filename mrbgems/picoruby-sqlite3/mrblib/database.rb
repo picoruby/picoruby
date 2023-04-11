5f5fb7dda99568f6a065261e533ab8ba3bf7a311
HASUMI Hitoshi <hasumikin@gmail.com>
2023-04-11 14:01:52 +0900

    Awful bug

--- mrbgems/picoruby-sqlite3/mrblib/database.rb
+++ mrbgems/picoruby-sqlite3/mrblib/database.rb
@@ -5,7 +5,7 @@ class SQLite3
   class Database
     class << self
       def new(filename)
-        volume, path = VFS.sanitize_and_split("/home/test.db")
+        volume, path = VFS.sanitize_and_split(filename)
         SQLite3.vfs_methods = volume[:driver].class.vfs_methods
         SQLite3::Database._open "#{volume[:driver].prefix}#{path}"
       end
