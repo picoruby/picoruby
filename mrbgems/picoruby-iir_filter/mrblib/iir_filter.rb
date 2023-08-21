55005314622a7a7d4195dd98d829dbe4d2869db0
HASUMI Hitoshi <hasumikin@gmail.com>
2023-08-21 17:47:34 +0900

    fix IIRFilter

--- mrbgems/picoruby-iir_filter/mrblib/iir_filter.rb
+++ mrbgems/picoruby-iir_filter/mrblib/iir_filter.rb
@@ -3,24 +3,23 @@
 class IIRFilter
   COEFF_SHIFT = 4
   MAX_COEFF_SHIFT = 8
+  FILTER_FEEDFORWARD = 100 * 256
 
-  def initialize(bit_depth)
-    if bit_depth < 6
-      raise ArgumentError, "Bit depth must be greater than 6"
-    end
-    @feedforward = 0b11 << bit_depth - 7
+  def initialize
     @filterd_value = 0
   end
 
   def filter(raw_value)
     # Left shift input by 256 to allow divisions up to 256
     raw_value <<= MAX_COEFF_SHIFT
-    if raw_value < (@filterd_value - @feedforward) || (@filterd_value + @feedforward) < raw_value
+    if raw_value < (@filterd_value - FILTER_FEEDFORWARD) || (@filterd_value + FILTER_FEEDFORWARD) < raw_value
       # Pass the filter input as it is for fast changes in input
       @filterd_value = raw_value
+      puts "case 1 filterd_value: #{@filterd_value}"
     else
       # If not the first sample then based on filter coefficient, filter the input signal
       @filterd_value += (raw_value - @filterd_value) >> COEFF_SHIFT
+      puts "case 2 filterd_value: #{@filterd_value}"
     end
     # Compensate filter result for left shift of 8 and round off
     return (@filterd_value >> MAX_COEFF_SHIFT) + ((@filterd_value >> MAX_COEFF_SHIFT - 1) & 1)
