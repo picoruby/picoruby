e5e1457ddc7444566e3f921adec8accedc12fa57
HASUMI Hitoshi <hasumikin@gmail.com>
2023-09-12 10:14:17 +0900

    [CYW43] CYW43.check_initialized

--- mrbgems/picoruby-cyw43/mrblib/cyw43.rb
+++ mrbgems/picoruby-cyw43/mrblib/cyw43.rb
@@ -3,12 +3,21 @@ class CYW43
     if (!country.is_a?(String) || country.length != 2)
       raise ArgumentError, "country must be a 2-character string"
     end
-    self._init(country.upcase, force)
+    if self._init(country.upcase, force)
+      $_cyw43_country = country
+    end
+  end
+
+  def self.check_initialized
+    unless $_cyw43_country
+      raise RuntimeError, "CYW43.init must be called before using GPIO"
+    end
   end
 
   class GPIO
     LED_PIN = 0
     def initialize(pin)
+      CYW43.check_initialized
       @pin = pin
     end
   end
