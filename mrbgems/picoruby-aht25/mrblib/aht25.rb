e4de00e9fb64fa341b4815223b938f05f469d063
HASUMI Hitoshi <hasumikin@gmail.com>
2023-11-08 14:34:12 +0900

    [AHT25] fixed

--- mrbgems/picoruby-aht25/mrblib/aht25.rb
+++ mrbgems/picoruby-aht25/mrblib/aht25.rb
@@ -10,26 +10,33 @@ class AHT25
   end
 
   def check
-    while true
-      @i2c.write(ADDRESS, 0x71)
-      status = @i2c.read(ADDRESS, 1).bytes[0]
-      break if status & 0x18 == 0x18
-      [0x1b, 0x1c, 0x1e].each do |reg|
-        @i2c.write(ADDRESS, reg, 0x71)
-        sleep_ms 10
-      end
-      sleep_ms 100
-    end
+    @i2c.write(ADDRESS, 0x71)
     sleep_ms 10
+    if @i2c.read(ADDRESS, 1)[0]&.ord & 0x18 == 0x18
+      true
+    else
+      puts "AHT25: check failed. You may need to reset."
+      false
+    end
+  end
+
+  def reset
+    [0x1b, 0x1c, 0x1e].each do |reg|
+      @i2c.write(ADDRESS, reg, 0x71)
+      sleep_ms 10
+    end
+    sleep_ms 100
   end
 
   def read
     @i2c.write(ADDRESS, 0xAC, 0x33, 0x00)
     sleep_ms 80
-    data = [0]
-    while data[0] & 0b10000000 == 0
+    while true
       data = @i2c.read(ADDRESS, 7).bytes # data[6] is checksum
+      break if data[0] & 0b10000000 == 0
+      sleep_ms 10
     end
+    raise "AHT25: read failed" unless data
     hum = data[1] << 12 | data[2] << 4 | ((data[3] & 0xF0) >> 4)
     tmp = ((data[3] & 0x0F) << 16) | data[4] << 8 | data[5]
     return({
