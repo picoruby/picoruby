c8b36f464994130ada80207d804f65471b7d0455
HASUMI Hitoshi <hasumikin@gmail.com>
2024-03-10 16:07:02 +0900

    MRUBY_CONFIG=mrbc2

--- build_config/r2p2-bin.rb
+++ build_config/r2p2-bin.rb
@@ -12,6 +12,7 @@ MRuby::CrossBuild.new("no-libc-host") do |conf|
   conf.cc.defines << "LEMON_PICORBC=1"
   conf.cc.defines << "MRBC_INT64"
   conf.cc.defines << "MRBC_USE_HAL_POSIX"
+  conf.cc.defines << 'PRISM_CUSTOM_ALLOCATOR'
 
   conf.gembox "r2p2"
   conf.gem core: "picoruby-bin-r2p2"
