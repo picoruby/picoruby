d153a3c804b38d1605f1d0d4b555448f93e7de23
Ryo Kajiwara <sylph01@s01.ninja>
2024-04-08 20:07:50 +0900

    decode64 initial

--- mrbgems/picoruby-base64/src/base64.c
+++ mrbgems/picoruby-base64/src/base64.c
@@ -64,23 +64,29 @@ c_base64_encode(mrbc_vm *vm, mrbc_value *v, int argc)
 
 unsigned char c_base64_convert(unsigned char n) {
   if (n >= 0x30 && n <= 0x39) {
-    return n - 0x30;
-  } else if (n >= 0x41 && n <= 0x46) {
-    return n - 55; // 0x41(A) = 65 should return 10
-  } else if (n >= 0x61 && n <= 0x66) {
-    return n - 87; // 0x61(a) = 97 should return 10
+    return n + 4;  // 0x30(0) = 48 should return 52
+  } else if (n >= 0x41 && n <= 0x5a) {
+    return n - 65; // 0x41(A) = 65 should return 0
+  } else if (n >= 0x61 && n <= 0x7a) {
+    return n - 71; // 0x61(a) = 97 should return 26
+  } else if (n == 0x2b) {
+    return 62; // +
+  } else if (n == 0x2f) {
+    return 63; // slash
   } else {
     return 0;
   }
 }
 
-/*
 static void
 c_base64_decode(mrbc_vm *vm, mrbc_value *v, int argc)
 {
   size_t index = 0;
+  size_t out_index = 0;
   size_t in_size, out_size;
-  uint8_t n0, n1;
+  size_t actual_out_size = 0;
+  uint32_t buf = 0;
+  int iter = 0;
 
   mrbc_value input = GET_ARG(1);
   if (input.tt != MRBC_TT_STRING) {
@@ -89,29 +95,39 @@ c_base64_decode(mrbc_vm *vm, mrbc_value *v, int argc)
   }
 
   in_size  = input.string->size;
-  out_size = in_size / 2;
-  // Base16 in RFC4648 assumes input size to be even,
-  // but Ruby's pack('H*') treats as if there is a 0 at the end
-  if (in_size % 2 == 1) {
-    out_size += 1;
-  }
+  out_size = (in_size + 3) / 4 * 3 + 1;
+  uint8_t* data   = input.string->data;
   uint8_t* output = mrbc_alloc(vm, out_size);
 
-  for (index = 0; index < out_size; index++) {
-    n0 = convert(input.string->data[index * 2]);
-    if (index * 2 + 1 < in_size) {
-      n1 = convert(input.string->data[index * 2 + 1]);
-    } else {
-      n1 = 0;
+  for (; index < in_size; index++) {
+    unsigned char c = c_base64_convert(data[index]);
+    if (c == '=')
+      break;
+    buf = buf << 6 | c;
+    iter++;
+    if (iter == 4) {
+      output[out_index++] = (buf >> 16) & 255;
+      output[out_index++] = (buf >> 8)  & 255;
+      output[out_index++] = buf         & 255;
+      actual_out_size += 3;
+      buf = 0;
+      iter = 0;
     }
-    output[index] = (n0 << 4) + n1;
   }
 
-  mrbc_value ret = mrbc_string_new(vm, output, out_size);
+  if (iter == 3) {
+    output[out_index++] = (buf >> 10) & 255;
+    output[out_index++] = (buf >>  2) & 255;
+    actual_out_size += 2;
+  } else if (iter == 2) {
+    output[out_index++] = (buf >> 4) & 255;
+    actual_out_size += 1;
+  }
+
+  mrbc_value ret = mrbc_string_new(vm, output, actual_out_size);
   mrbc_free(vm, output);
   SET_RETURN(ret);
 }
-*/
 
 void
 mrbc_base64_init(void)
@@ -119,5 +135,5 @@ mrbc_base64_init(void)
   mrbc_class *class_Base64 = mrbc_define_class(0, "Base64", mrbc_class_object);
 
   mrbc_define_method(0, class_Base64, "encode64", c_base64_encode);
-  // mrbc_define_method(0, class_Base64, "decode64", c_base64_decode);
+  mrbc_define_method(0, class_Base64, "decode64", c_base64_decode);
 }
