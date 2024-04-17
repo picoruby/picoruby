1777be1873e01cafcad8f0efe33bf31d015d1804
Ryo Kajiwara <sylph01@s01.ninja>
2024-04-17 18:13:04 +0900

    Random Number Generator gem

---
+++ mrbgems/picoruby-rng/ports/rp2040/rng.c
@@ -0,0 +1,20 @@
+#include "../../include/rng.h"
+#include "hardware/structs/rosc.h"
+#include "pico/time.h"
+
+uint8_t rng_random_byte(void)
+{
+  uint32_t random = 0;
+  uint32_t bit = 0;
+  for (int i = 0; i < 8; i++) {
+    while (true) {
+      bit = rosc_hw->randombit;
+      sleep_us(5);
+      if (bit != rosc_hw->randombit)
+        break;
+    }
+    random = (random << 1) | bit;
+    sleep_us(5);
+  }
+  return (uint8_t) random;
+}
---
+++ mrbgems/picoruby-rng/sig/rng.rbs
@@ -0,0 +1,2 @@
+class RNG
+end
