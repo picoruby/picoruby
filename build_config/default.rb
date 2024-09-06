4d9bc3673edd156d5fcde2f4e9f290c33745a03f
HASUMI Hitoshi <hasumikin@gmail.com>
2024-09-06 19:43:01 +0900

    default target includes r2p2-bin

--- build_config/default.rb
+++ build_config/default.rb
@@ -5,6 +5,8 @@ MRuby::Build.new do |conf|
   conf.picoruby
 
   conf.gembox "picoruby"
+  conf.gembox "r2p2"
+  conf.gem core: "picoruby-bin-r2p2"
 
   ENV['MRUBYC_BRANCH'] ||= "master"
   ENV['MRUBYC_REVISION'] ||= "52fcc668a3a175f6fd2c991d5c3b5f8eca927604"
