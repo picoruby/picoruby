1b498e2e897c28aabd2ece81a0d62215d4075869
HASUMI Hitoshi <hasumikin@gmail.com>
2024-09-30 15:24:25 +0900

    urlsafe_encode64 and urlsafe_decode64

--- mrbgems/picoruby-base64/mrblib/base64.rb
+++ mrbgems/picoruby-base64/mrblib/base64.rb
@@ -1,2 +1,9 @@
 class Base64
+  def self.urlsafe_encode64(string)
+    Base64.encode64(string).tr('+/', '-_').tr('=', '')
+  end
+
+  def self.urlsafe_decode64(string)
+    Base64.decode64(string.tr('-_', '+/').ljust((string.length + 3) & ~3, '='))
+  end
 end
