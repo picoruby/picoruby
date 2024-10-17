933e92b3266c828acc388f555a19856d340f7e17
HASUMI Hitoshi <hasumikin@gmail.com>
2024-10-17 13:27:40 +0900

    [picoruby-io] wip. IO.popen

--- mrbgems/picoruby-io/mrblib/io.rb
+++ mrbgems/picoruby-io/mrblib/io.rb
@@ -1,7 +1,16 @@
+##
+# IO
+#
+# ISO 15.2.20
+
+require "metaprog"
 
 class EOFError < IOError; end
 
 class IO
+
+  include Enumerable
+
   def self.open(*args, &block)
     io = self.new(*args)
 
@@ -15,6 +24,128 @@ class IO
       rescue StandardError
       end
     end
+  end
+
+  def self.popen(command, mode = 'r', **opts, &block)
+    if !self.respond_to?(:_popen)
+      raise NotImplementedError, "popen is not supported on this platform"
+    end
+    opt_in  = opts[:in]  || 0
+    opt_out = opts[:out] || 1
+    opt_err = opts[:err] || 2
+    io = self._popen(command, mode, opt_in, opt_out, opt_err)
+    return io unless block
+
+    begin
+      yield io
+    ensure
+      begin
+        io.close unless io.closed?
+      rescue IOError
+        # nothing
+      end
+    end
+  end
+
+  def self.pipe(&block)
+    if !self.respond_to?(:_pipe)
+      raise NotImplementedError, "pipe is not supported on this platform"
+    end
+    if block
+      begin
+        r, w = IO._pipe
+        yield r, w
+      ensure
+        r.close unless r.closed?
+        w.close unless w.closed?
+      end
+    else
+      IO._pipe
+    end
+  end
+
+  def self.read(path, length=nil, offset=0, mode: "r")
+    str = ""
+    fd = -1
+    io = nil
+    begin
+      fd = IO.sysopen(path, mode)
+      io = IO.open(fd, mode)
+      io.seek(offset) if offset > 0
+      str = io.read(length)
+    ensure
+      if io
+        io.close
+      elsif fd != -1
+        IO._sysclose(fd)
+      end
+    end
+    str
+  end
+
+  def hash
+    # We must define IO#hash here because IO includes Enumerable and
+    # Enumerable#hash will call IO#read() otherwise
+    self.__id__
+  end
+
+  def <<(str)
+    write(str)
+    self
+  end
+
+  alias eof eof?
+  alias tell pos
+
+  def pos=(i)
+    seek(i, SEEK_SET)
+  end
+
+  def rewind
+    seek(0, SEEK_SET)
+  end
+
+  def ungetbyte(c)
+    if c.is_a? String
+      c = c.getbyte(0)
+    else
+      c &= 0xff
+    end
+    s = " "
+    s.setbyte(0,c)
+    ungetc s
+  end
+
+  # 15.2.20.5.3
+  def each(&block)
+    return to_enum unless block
+
+    while line = self.gets
+      block.call(line)
+    end
+    self
+  end
+
+  # 15.2.20.5.4
+  def each_byte(&block)
+    return to_enum(:each_byte) unless block
+
+    while byte = self.getbyte
+      block.call(byte)
+    end
+    self
+  end
+
+  # 15.2.20.5.5
+  alias each_line each
+
+  def each_char(&block)
+    return to_enum(:each_char) unless block
+
+    while char = self.getc
+      block.call(char)
+    end
+    self
   end
 
   def puts(*args)
@@ -34,4 +165,29 @@ class IO
     write "\n" if len == 0
     nil
   end
+
+  def print(*args)
+    i = 0
+    len = args.size
+    while i < len
+      write args[i].to_s
+      i += 1
+    end
+  end
+
+  def printf(*args)
+    write sprintf(*args)
+    nil
+  end
+
+  alias to_i fileno
+  alias tty? isatty
 end
+
+STDIN  = IO.open(0, "r")
+STDOUT = IO.open(1, "w")
+STDERR = IO.open(2, "w")
+
+$stdin  = STDIN
+$stdout = STDOUT
+$stderr = STDERR
