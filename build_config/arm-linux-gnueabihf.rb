325d995a95a4267f25e9fc383d8acf4a9647f80e
HASUMI Hitoshi <hasumikin@gmail.com>
2024-11-03 17:48:11 +0900

    wip cross-build

--- build_config/arm-linux-gnueabihf.rb
+++ build_config/arm-linux-gnueabihf.rb
@@ -9,6 +9,8 @@ MRuby::CrossBuild.new("arm-linux-gnueabihf") do |conf|
   conf.cc.flags << '-D_GNU_SOURCE'
   conf.linker.command = 'arm-linux-gnueabihf-gcc'
   conf.linker.libraries = %w(m c gcc resolv)
+  conf.linker.flags_after_libraries = '-lssl -lcrypto -ldl'
+
   conf.linker.flags << '-Wl,-rpath,/usr/arm-linux-gnueabihf/lib'
   conf.archiver.command = 'arm-linux-gnueabihf-ar'
 
@@ -30,10 +32,6 @@ MRuby::CrossBuild.new("arm-linux-gnueabihf") do |conf|
   conf.gembox "posix"
   conf.gembox "stdlib"
   conf.gembox "utils"
-
-  conf.linker.flags_after_libraries << '-lssl'
-  conf.linker.flags_after_libraries << '-lcrypto'
-  conf.linker.flags_after_libraries << '-ldl'
 
   conf.picoruby
 
