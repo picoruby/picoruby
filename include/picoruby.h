d6a4e1d82c4d71eb8d09e1f5a54fa16525caf002
HASUMI Hitoshi <hasumikin@gmail.com>
2025-02-18 10:35:59 +0900

    [WIP] task scheduler

--- include/picoruby.h
+++ include/picoruby.h
@@ -71,11 +71,24 @@ bool picoruby_load_model_by_name(const char *gem);
 
 #elif defined(PICORB_VM_MRUBY)
 
+#if !defined(MRB_USE_TASK_SCHEDULER)
+#define MRB_USE_TASK_SCHEDULER 1
+#endif
+#if !defined(MRB_USE_VM_SWITCH_DISPATCH)
+#define MRB_USE_VM_SWITCH_DISPATCH 1
+#endif
+
 #define picorb_state mrb_state
 #define picorb_value mrb_value
 
 #undef mrb_state
+#undef RITE_COMPILER_NAME
+
+#if !defined(MRUBY_IREP_H)
 #define MRUBY_IREP_H 1
+#endif
+#define mrb_irep mrc_irep // should be compatible
+
 #include <mruby.h>
 #include <mruby/value.h>
 #include <mruby/proc.h>
