b154f5a31cafb5e3d76c4e9a8e94976f1d85e1dc
HASUMI Hitoshi <hasumikin@gmail.com>
2025-03-08 17:33:04 +0900

    Fix File::Stat

--- mrbgems/mruby-file-stat/mrblib/file-stat.rb
+++ mrbgems/mruby-file-stat/mrblib/file-stat.rb
@@ -1,16 +1,14 @@
-class File
-  class Stat
-    def initialize(entry)
-      @entry = entry
-    end
-    def size
-      FileTest.size(@entry)
-    end
-    def directory?
-      FileTest.directory?(@entry)
-    end
-    def mtime
-      File.new(@entry).mtime
-    end
+class File::Stat
+  def initialize(entry)
+    @entry = entry
+  end
+  def size
+    FileTest.size(@entry)
+  end
+  def directory?
+    FileTest.directory?(@entry)
+  end
+  def mtime
+    File.new(@entry).mtime
   end
 end
