dab36be835362336c7fd994cdb04a82bb98c5073
HASUMI Hitoshi <hasumikin@gmail.com>
2025-03-13 19:31:39 +0900

    Task name && Task.stat

--- mrbgems/picoruby-shell/shell_executables/r2p2.rb
+++ mrbgems/picoruby-shell/shell_executables/r2p2.rb
@@ -1,11 +1,11 @@
 load "/etc/init.d/r2p2_wifi"
 
-if File.exist?("/home/app.mrb")
+if File.exist?("#{ENV['HOME']}/app.mrb")
   puts "Loading app.mrb"
-  load "/home/app.mrb"
-elsif File.exist?("/home/app.rb")
+  load "#{ENV['HOME']}/app.mrb"
+elsif File.exist?("#{ENV['HOME']}/app.rb")
   puts "Loading app.rb"
-  load "/home/app.rb"
+  load "#{ENV['HOME']}/app.rb"
 else
   puts "No app.(mrb|rb) found"
 end
