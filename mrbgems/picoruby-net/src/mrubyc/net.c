94ffaf517bbbb12e8036263e48f5131c19242fec
HASUMI Hitoshi <hasumikin@gmail.com>
2025-03-31 11:29:32 +0900

    WIP porting picoruby-net

--- mrbgems/picoruby-net/src/mrubyc/net.c
+++ mrbgems/picoruby-net/src/mrubyc/net.c
@@ -1,3 +1,11 @@
+#include "mrubyc.h"
+
+void
+mbedtls_debug(void *ctx, int level, const char *file, int line, const char *str)
+{
+  ((void) level);
+  console_printf("%s:%04d: %s", file, line, str);
+}
 
 static void
 c_net_dns_resolve(mrbc_vm *vm, mrbc_value *v, int argc)
@@ -13,7 +21,7 @@ c_net_dns_resolve(mrbc_vm *vm, mrbc_value *v, int argc)
   }
 
   char ipaddr[16] = {0};
-  DNS_resolve((const char *)GET_STRING_ARG(1), (GET_ARG(2).tt == MRBC_TT_TRUE), ipaddr);
+  DNS_resolve((const char *)GET_STRING_ARG(1), ipaddr, (GET_ARG(2).tt == MRBC_TT_TRUE));
   if (ipaddr[0] != '\0') {
     mrbc_value ret = mrbc_string_new_cstr(vm, ipaddr);
     SET_RETURN(ret);
@@ -39,19 +47,29 @@ c_net_tcpclient__request_impl(mrbc_vm *vm, mrbc_value *v, int argc)
     mrbc_raise(vm, MRBC_CLASS(TypeError), "wrong type of argument");
     return;
   }
-  mrbc_value input = GET_ARG(3);
-  if (input.tt != MRBC_TT_STRING) {
+  if (v[3].tt != MRBC_TT_STRING) {
     mrbc_raise(vm, MRBC_CLASS(TypeError), "wrong type of argument");
     return;
   }
+  const char *send_data = (const char *)GET_STRING_ARG(3);
+  size_t send_data_len = v[3].string->size;
   mrbc_value is_tls = GET_ARG(4);
   if (!((is_tls.tt == MRBC_TT_TRUE) || (is_tls.tt == MRBC_TT_FALSE))) {
     mrbc_raise(vm, MRBC_CLASS(TypeError), "wrong type of argument");
     return;
   }
 
-  mrbc_value ret = TCPClient_send((const char *)GET_STRING_ARG(1), port.i, &input, (is_tls.tt == MRBC_TT_TRUE));
-  SET_RETURN(ret);
+  recv_data_t *recv_data = (recv_data_t *)mrbc_alloc(vm, sizeof(recv_data_t));
+  recv_data->data = NULL;
+  recv_data->len = 0;
+  TCPClient_send((const char *)GET_STRING_ARG(1), port.i, send_data, send_data_len, (is_tls.tt == MRBC_TT_TRUE), recv_data);
+  if (recv_data->data == NULL) {
+    SET_NIL_RETURN();
+  } else {
+    mrbc_value ret = mrbc_string_new_alloc(vm, recv_data->data, recv_data->len);
+    SET_RETURN(ret);
+  }
+  mrbc_free(vm, recv_data);
 }
 
 static void
@@ -71,19 +89,29 @@ c_net_udpclient__send_impl(mrbc_vm *vm, mrbc_value *v, int argc)
     mrbc_raise(vm, MRBC_CLASS(TypeError), "wrong type of argument for port");
     return;
   }
-  mrbc_value data = GET_ARG(3);
-  if (data.tt != MRBC_TT_STRING) {
+  if (v[3].tt != MRBC_TT_STRING) {
     mrbc_raise(vm, MRBC_CLASS(TypeError), "wrong type of argument for data");
     return;
   }
+  const char* data = (const char *)GET_STRING_ARG(3);
+  size_t data_len = v[3].string->size;
   mrbc_value use_dtls = GET_ARG(4);
   if (!((use_dtls.tt == MRBC_TT_TRUE) || (use_dtls.tt == MRBC_TT_FALSE))) {
     mrbc_raise(vm, MRBC_CLASS(TypeError), "wrong type of argument for use_dtls");
     return;
   }
 
-  mrbc_value ret = UDPClient_send((const char *)GET_STRING_ARG(1), port.i, &data, (use_dtls.tt == MRBC_TT_TRUE));
-  SET_RETURN(ret);
+  recv_data_t *recv_data = (recv_data_t *)mrbc_alloc(vm, sizeof(recv_data_t));
+  recv_data->data = NULL;
+  recv_data->len = 0;
+  UDPClient_send((const char *)GET_STRING_ARG(1), port.i, data, data_len, (use_dtls.tt == MRBC_TT_TRUE), recv_data);
+  if (recv_data->data == NULL) {
+    SET_NIL_RETURN();
+  } else {
+    mrbc_value ret = mrbc_string_new_alloc(vm, recv_data->data, recv_data->len);
+    SET_RETURN(ret);
+  }
+  mrbc_free(vm, recv_data);
 }
 
 void
--- mrbgems/picoruby-net/src/tcp.c
+++ mrbgems/picoruby-net/src/tcp.c
@@ -1,3 +1,5 @@
+#include "../include/net.h"
+
 /* state values for TCP connection */
 #define NET_TCP_STATE_NONE               0
 #define NET_TCP_STATE_CONNECTION_STARTED 1
@@ -13,20 +15,13 @@ typedef struct tcp_connection_state_str
 {
   int state;
   struct altcp_pcb *pcb;
-  mrbc_value *send_data;
-  mrbc_value *recv_data;
+  const char *send_data;
+  size_t send_data_len;
+  recv_data_t *recv_data;
   struct altcp_tls_config *tls_config;
 } tcp_connection_state;
 
 /* end of platform-dependent definitions */
-
-static void
-TCPClient_free_tls_config(tcp_connection_state *cs)
-{
-  if (cs && cs->tls_config) {
-    altcp_tls_free_config(cs->tls_config);
-  }
-}
 
 static err_t
 TCPClient_close(tcp_connection_state *cs)
@@ -41,13 +36,15 @@ TCPClient_close(tcp_connection_state *cs)
   altcp_poll(cs->pcb, NULL, 0);
   err = altcp_close(cs->pcb);
   if (err != ERR_OK) {
-    console_printf("altcp_close failed: %d\n", err);
+    picorb_warn("altcp_close failed: %d\n", err);
     altcp_abort(cs->pcb);
     err = ERR_ABRT;
   }
   lwip_end();
-  TCPClient_free_tls_config(cs);
-  mrbc_raw_free(cs);
+  if (cs->tls_config) {
+    altcp_tls_free_config(cs->tls_config);
+  }
+  picorb_free(cs);
   return err;
 }
 
@@ -56,22 +53,26 @@ TCPClient_recv_cb(void *arg, struct altcp_pcb *pcb, struct pbuf *pbuf, err_t err
 {
   tcp_connection_state *cs = (tcp_connection_state *)arg;
   if (err != ERR_OK) {
-    console_printf("TCPClient_recv_cb: err=%d\n", err);
+    picorb_warn("TCPClient_recv_cb: err=%d\n", err);
     cs->state = NET_TCP_STATE_ERROR;
     return TCPClient_close(cs);
   }
   if (pbuf != NULL) {
-    char *tmpbuf = mrbc_raw_alloc(pbuf->tot_len + 1);
+    recv_data_t *recv_data = cs->recv_data;
+    if (recv_data->data == NULL) {
+      recv_data->data = (char *)picorb_alloc(pbuf->tot_len + 1);
+    } else {
+      recv_data->data = (char *)picorb_realloc(recv_data->data, pbuf->tot_len + 1);
+    }
     struct pbuf *current_pbuf = pbuf;
-    int offset = 0;
+    size_t offset = recv_data->len;
     while (current_pbuf != NULL) {
-      pbuf_copy_partial(current_pbuf, tmpbuf + offset, current_pbuf->len, 0);
+      pbuf_copy_partial(current_pbuf, recv_data->data + offset, current_pbuf->len, 0);
       offset += current_pbuf->len;
       current_pbuf = current_pbuf->next;
     }
-    tmpbuf[pbuf->tot_len] = '\0';
-    mrbc_string_append_cbuf(cs->recv_data, tmpbuf, pbuf->tot_len);
-    mrbc_raw_free(tmpbuf);
+    recv_data->data[pbuf->tot_len] = '\0';
+    recv_data->len += pbuf->tot_len;
     altcp_recved(pcb, pbuf->tot_len);
     cs->state = NET_TCP_STATE_PACKET_RECVED;
     pbuf_free(pbuf);
@@ -92,7 +93,7 @@ static err_t
 TCPClient_connected_cb(void *arg, struct altcp_pcb *pcb, err_t err)
 {
   if (err != ERR_OK) {
-    console_printf("TCPClient_connected_cb: err=%d\n", err);
+    picorb_warn("TCPClient_connected_cb: err=%d\n", err);
     return TCPClient_close((tcp_connection_state *)arg);
   }
   tcp_connection_state *cs = (tcp_connection_state *)arg;
@@ -103,7 +104,7 @@ TCPClient_connected_cb(void *arg, struct altcp_pcb *pcb, err_t err)
 static err_t
 TCPClient_poll_cb(void *arg, struct altcp_pcb *pcb)
 {
-  console_printf("TCPClient_poll_cb (timeout)\n");
+  picorb_warn("TCPClient_poll_cb (timeout)\n");
   tcp_connection_state *cs = (tcp_connection_state *)arg;
   cs->state = NET_TCP_STATE_TIMEOUT;
   return TCPClient_close(cs);
@@ -113,75 +114,56 @@ static void
 TCPClient_err_cb(void *arg, err_t err)
 {
   tcp_connection_state *cs = (tcp_connection_state *)arg;
-  console_printf("Error with: %d\n", err);
+  picorb_warn("Error with: %d\n", err);
   cs->state = NET_TCP_STATE_ERROR;
 }
 
 static tcp_connection_state *
-TCPClient_new_connection(mrbc_value *send_data, mrbc_value *recv_data)
+TCPClient_new_connection(const char *host, bool is_tls, const char *send_data, size_t send_data_len, recv_data_t *recv_data)
 {
-  tcp_connection_state *cs = (tcp_connection_state *)mrbc_raw_alloc(sizeof(tcp_connection_state));
-  cs->tls_config = NULL;
-  cs->state = NET_TCP_STATE_NONE;
-  cs->pcb = altcp_new(NULL);
-  altcp_recv(cs->pcb, TCPClient_recv_cb);
-  altcp_sent(cs->pcb, TCPClient_sent_cb);
-  altcp_err(cs->pcb, TCPClient_err_cb);
-  altcp_poll(cs->pcb, TCPClient_poll_cb, 10);
-  altcp_arg(cs->pcb, cs);
-  cs->send_data = send_data;
-  cs->recv_data = recv_data;
-  return cs;
-}
+  tcp_connection_state *cs = (tcp_connection_state *)picorb_alloc(sizeof(tcp_connection_state));
 
-static tcp_connection_state *
-TCPClient_new_tls_connection(const char *host, mrbc_value *send_data, mrbc_value *recv_data)
-{
-  tcp_connection_state *cs = (tcp_connection_state *)mrbc_raw_alloc(sizeof(tcp_connection_state));
-  cs->state = NET_TCP_STATE_NONE;
-
-  struct altcp_tls_config *tls_config = altcp_tls_create_config_client(NULL, 0);
-  cs->pcb = altcp_tls_new(tls_config, IPADDR_TYPE_V4);
-  if (!cs->pcb) {
-    console_printf("altcp_tls_new failed\n");
-    mrbc_raw_free(cs);
-    return NULL;
+  if (is_tls) {
+    struct altcp_tls_config *tls_config = altcp_tls_create_config_client(NULL, 0);
+    cs->pcb = altcp_tls_new(tls_config, IPADDR_TYPE_V4);
+    if (!cs->pcb) {
+      picorb_free(cs);
+      return NULL;
+    }
+    cs->tls_config = tls_config;
+    mbedtls_ssl_set_hostname(altcp_tls_context(cs->pcb), host);
+  } else {
+    (void)host;
+    cs->pcb = altcp_new(NULL);
+    cs->tls_config = NULL;
   }
-  cs->tls_config = tls_config;
-  mbedtls_ssl_set_hostname(altcp_tls_context(cs->pcb), host);
+
+  cs->state = NET_TCP_STATE_NONE;
   altcp_recv(cs->pcb, TCPClient_recv_cb);
   altcp_sent(cs->pcb, TCPClient_sent_cb);
   altcp_err(cs->pcb, TCPClient_err_cb);
   altcp_poll(cs->pcb, TCPClient_poll_cb, 10);
   altcp_arg(cs->pcb, cs);
   cs->send_data = send_data;
+  cs->send_data_len = send_data_len;
   cs->recv_data = recv_data;
   return cs;
 }
 
-static tcp_connection_state *
-TCPClient_connect_impl(ip_addr_t *ip, const char *host, int port, mrbc_value *send_data, mrbc_value *recv_data, bool is_tls)
+static err_t
+TCPClient_connect_impl(tcp_connection_state *cs, ip_addr_t *ip, int port)
 {
   err_t err;
-  tcp_connection_state *cs;
-  if (is_tls) {
-    cs = TCPClient_new_tls_connection(host, send_data, recv_data);
-  } else {
-    cs = TCPClient_new_connection(send_data, recv_data);
-  }
-  if (cs) {
-    lwip_begin();
-    err = altcp_connect(cs->pcb, ip, port, TCPClient_connected_cb);
-    if (err != ERR_OK) {
-      console_printf("altcp_connect failed: %d\n", err);
-      cs->state = NET_TCP_STATE_ERROR;
-      lwip_end();
-      return cs;
-    }
+  lwip_begin();
+  err = altcp_connect(cs->pcb, ip, port, TCPClient_connected_cb);
+  if (err != ERR_OK) {
+    cs->state = NET_TCP_STATE_ERROR;
     lwip_end();
-    cs->state = NET_TCP_STATE_CONNECTION_STARTED;
+    return err;
   }
-  return cs;
+  lwip_end();
+  cs->state = NET_TCP_STATE_CONNECTION_STARTED;
+  return err;
 }
 
 static int
@@ -202,9 +184,9 @@ TCPClient_poll_impl(tcp_connection_state **pcs)
     case NET_TCP_STATE_CONNECTED:
       cs->state = NET_TCP_STATE_WAITING_PACKET;
       lwip_begin();
-      err = altcp_write(cs->pcb, cs->send_data->string->data, cs->send_data->string->size, 0);
+      err = altcp_write(cs->pcb, cs->send_data, cs->send_data_len, 0);
       if (err != ERR_OK) {
-        console_printf("altcp_write failed: %d\n", err);
+        picorb_warn("altcp_write failed: %d\n", err);
         cs->state = NET_TCP_STATE_ERROR;
         return 1;
       }
@@ -227,30 +209,29 @@ TCPClient_poll_impl(tcp_connection_state **pcs)
   return cs->state;
 }
 
-mrbc_value
-TCPClient_send(const char *host, int port, mrbc_value *send_data, bool is_tls)
+void
+TCPClient_send(const char *host, int port, const char *send_data, size_t send_data_len, bool is_tls, recv_data_t *recv_data)
 {
   ip_addr_t ip;
   ip4_addr_set_zero(&ip);
-  mrbc_value ret;
   Net_get_ip(host, &ip);
   if(!ip4_addr_isloopback(&ip)) {
     char ip_str[16];
     ipaddr_ntoa_r(&ip, ip_str, 16);
-    mrbc_value recv_data = mrbc_string_new(NULL, NULL, 0);
-    tcp_connection_state *cs = TCPClient_connect_impl(&ip, host, port, send_data, &recv_data, is_tls);
-    if (!cs) {
-      ret = mrbc_nil_value();
+    tcp_connection_state *cs = TCPClient_new_connection(host, is_tls, send_data, send_data_len, recv_data);
+    if (cs == NULL) {
+      picorb_warn("TCPClient_new_connection failed\n");
+      return;
+    }
+    err_t err = TCPClient_connect_impl(cs, &ip, port);
+    if (err != ERR_OK) {
+      picorb_warn("altcp_connect failed: %d\n", err);
+      return;
     } else {
-      while(TCPClient_poll_impl(&cs))
-      {
+      while (TCPClient_poll_impl(&cs)) {
         sleep_ms(200);
       }
       // recv_data is ready after connection is complete
-      ret = recv_data;
     }
-  } else {
-    ret = mrbc_nil_value();
   }
-  return ret;
 }
