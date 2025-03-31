337bf55129e226d12e7194eb69473cc8a4fc2341
HASUMI Hitoshi <hasumikin@gmail.com>
2025-03-31 18:44:42 +0900

    porting picoruby-mbedtls

--- mrbgems/picoruby-mbedtls/mrblib/digest.rb
+++ mrbgems/picoruby-mbedtls/mrblib/digest.rb
@@ -1,17 +1,4 @@
 module MbedTLS
   class Digest
-    SUPPORTED_ALGORITHMS = {
-      :none => 0x00,
-      :sha256 => 0x01
-    }
-
-    def self.new(algorithm)
-      unless SUPPORTED_ALGORITHMS[algorithm] != nil
-        raise ArgumentError, "Algorithm not supported: #{algorithm}"
-      end
-      instance = self._init_ctx(SUPPORTED_ALGORITHMS[algorithm])
-
-      return instance
-    end
   end
 end
