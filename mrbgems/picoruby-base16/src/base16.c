90fdef297f811ac2d6d170244f9b74d34a795d15
HASUMI Hitoshi <hasumikin@gmail.com>
2025-04-04 10:41:09 +0900

    port Base16

--- mrbgems/picoruby-base16/src/base16.c
+++ mrbgems/picoruby-base16/src/base16.c
@@ -1,40 +1,9 @@
-#include <mrubyc.h>
-#include <alloc.h>
 
 const char base16chars[] = "0123456789abcdef";
 
-static void
-c_base16_encode(mrbc_vm *vm, mrbc_value *v, int argc)
+static unsigned char
+base16_convert(unsigned char n)
 {
-  size_t index = 0;
-  size_t in_size, out_size;
-  uint8_t n, n0, n1;
-
-  mrbc_value input = GET_ARG(1);
-  if (input.tt != MRBC_TT_STRING) {
-    mrbc_raise(vm, MRBC_CLASS(TypeError), "wrong type of argument");
-    return;
-  }
-
-  in_size  = input.string->size;
-  out_size = in_size * 2;
-  uint8_t* output = mrbc_alloc(vm, out_size);
-
-  for (index = 0; index < in_size; index++) {
-    n = input.string->data[index];
-    n0 = (n >> 4) & 15;
-    n1 = n & 15;
-
-    output[index * 2] = base16chars[n0];
-    output[index * 2 + 1] = base16chars[n1];
-  }
-
-  mrbc_value ret = mrbc_string_new(vm, output, out_size);
-  mrbc_free(vm, output);
-  SET_RETURN(ret);
-}
-
-unsigned char c_base16_convert(unsigned char n) {
   if (n >= 0x30 && n <= 0x39) {
     return n - 0x30;
   } else if (n >= 0x41 && n <= 0x46) {
@@ -46,48 +15,13 @@ unsigned char c_base16_convert(unsigned char n) {
   }
 }
 
-static void
-c_base16_decode(mrbc_vm *vm, mrbc_value *v, int argc)
-{
-  size_t index = 0;
-  size_t in_size, out_size;
-  uint8_t n0, n1;
 
-  mrbc_value input = GET_ARG(1);
-  if (input.tt != MRBC_TT_STRING) {
-    mrbc_raise(vm, MRBC_CLASS(TypeError), "wrong type of argument");
-    return;
-  }
+#if defined(PICORB_VM_MRUBY)
 
-  in_size  = input.string->size;
-  out_size = in_size / 2;
-  // Base16 in RFC4648 assumes input size to be even,
-  // but Ruby's pack('H*') treats as if there is a 0 at the end
-  if (in_size % 2 == 1) {
-    out_size += 1;
-  }
-  uint8_t* output = mrbc_alloc(vm, out_size);
+#include "mruby/base16.c"
 
-  for (index = 0; index < out_size; index++) {
-    n0 = c_base16_convert(input.string->data[index * 2]);
-    if (index * 2 + 1 < in_size) {
-      n1 = c_base16_convert(input.string->data[index * 2 + 1]);
-    } else {
-      n1 = 0;
-    }
-    output[index] = (n0 << 4) + n1;
-  }
+#elif defined(PICORB_VM_MRUBYC)
 
-  mrbc_value ret = mrbc_string_new(vm, output, out_size);
-  mrbc_free(vm, output);
-  SET_RETURN(ret);
-}
+#include "mrubyc/base16.c"
 
-void
-mrbc_base16_init(mrbc_vm *vm)
-{
-  mrbc_class *class_Base16 = mrbc_define_class(vm, "Base16", mrbc_class_object);
-
-  mrbc_define_method(vm, class_Base16, "encode16", c_base16_encode);
-  mrbc_define_method(vm, class_Base16, "decode16", c_base16_decode);
-}
+#endif
