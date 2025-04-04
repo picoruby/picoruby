e2dcb7fa1b32d90cf70ebd7e1de5d084256eee63
HASUMI Hitoshi <hasumikin@gmail.com>
2025-04-04 17:02:54 +0900

    for Pico2 W

--- build_config/r2p2-microruby-cortex-m33.rb
+++ build_config/r2p2-microruby-cortex-m33.rb
@@ -1,4 +1,4 @@
-MRuby::CrossBuild.new("r2p2_w-microruby-cortex-m0plus") do |conf|
+MRuby::CrossBuild.new("r2p2-microruby-cortex-m33") do |conf|
 
   ###############################################################
   # You need following tools:
@@ -10,10 +10,9 @@ MRuby::CrossBuild.new("r2p2_w-microruby-cortex-m0plus") do |conf|
   conf.cc.defines << "MRB_TICK_UNIT=1"
   conf.cc.defines << "MRB_TIMESLICE_TICK_COUNT=10"
 
-  conf.cc.defines << "PICORB_ALLOC_ESTALLOC"
-  conf.cc.defines << "ESTALLOC_DEBUG"
-  conf.cc.defines << "MRB_32BIT"
   conf.cc.defines << "USE_FAT_FLASH_DISK=1"
+  conf.cc.defines << "PICORB_ALLOC_ESTALLOC"
+#  conf.cc.defines << "MRB_NAN_BOXING"
 
   conf.cc.command = "arm-none-eabi-gcc"
   conf.linker.command = "arm-none-eabi-ld"
@@ -23,9 +22,18 @@ MRuby::CrossBuild.new("r2p2_w-microruby-cortex-m0plus") do |conf|
   conf.cc.host_command = "gcc"
 
   conf.cc.flags.flatten!
-  conf.cc.flags << "-mcpu=cortex-m0plus"
+  conf.cc.flags << "-mcpu=cortex-m33"
+#  conf.cc.flags << "-march=armv8-m.main+fp+dsp"
+#  conf.cc.flags << "-mabi=aapcs-linux"
+#  conf.cc.flags << "-mfloat-abi=softfp"
   conf.cc.flags << "-mthumb"
+
+  conf.cc.flags << "-fno-strict-aliasing"
+  conf.cc.flags << "-fno-unroll-loops"
+  conf.cc.flags << "-mslow-flash-data"
+
   conf.cc.flags << "-fshort-enums"
+
   conf.cc.flags << "-Wall"
   conf.cc.flags << "-Wno-format"
   conf.cc.flags << "-Wno-unused-function"
@@ -57,19 +65,7 @@ MRuby::CrossBuild.new("r2p2_w-microruby-cortex-m0plus") do |conf|
   conf.gem core: "picoruby-watchdog"
   conf.gem core: "picoruby-rng"
 
-  conf.gem core: "picoruby-gpio"
-  conf.gem core: "picoruby-adc"
-  conf.gem core: "picoruby-i2c"
-  conf.gem core: "picoruby-pwm"
-  conf.gem core: "picoruby-spi"
-  conf.gem core: "picoruby-uart"
-
-  conf.gem core: "picoruby-mbedtls"
-  conf.gem core: "picoruby-net"
-  conf.gem core: "picoruby-base16"
-  conf.gem core: "picoruby-base64"
-  conf.gem core: "picoruby-ble"
-
   conf.microruby
 end
+
 
