97a46548b4ce1f326780a2c666e2612847eeef20
HASUMI Hitoshi <hasumikin@gmail.com>
2025-04-07 15:49:59 +0900

    WIP porting picoruby-ble

--- build_config/r2p2_w-cortex-m0plus.rb
+++ build_config/r2p2_w-cortex-m0plus.rb
@@ -47,6 +47,7 @@ MRuby::CrossBuild.new("r2p2_w-cortex-m0plus") do |conf|
   conf.gembox "utils"
   conf.gem core: 'picoruby-jwt'
   conf.gem core: 'picoruby-net'
+  conf.gem core: 'picoruby-mbedtls'
   # For some reason, picoruby-net doesn't have dependency on picoruby-cyw43
   conf.gem core: 'picoruby-cyw43'
   conf.gem core: 'picoruby-ble'
