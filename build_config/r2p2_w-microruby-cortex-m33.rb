e70c22c7b5e80e6da6f4c09cd263de3db4e13941
HASUMI Hitoshi <hasumikin@gmail.com>
2025-04-10 16:59:17 +0900

    WIP

--- build_config/r2p2_w-microruby-cortex-m33.rb
+++ build_config/r2p2_w-microruby-cortex-m33.rb
@@ -10,8 +10,9 @@ MRuby::CrossBuild.new("r2p2_w-microruby-cortex-m33") do |conf|
   conf.cc.defines << "MRB_TICK_UNIT=1"
   conf.cc.defines << "MRB_TIMESLICE_TICK_COUNT=10"
 
-  conf.cc.defines << "PICORB_ALLOC_ESTALLOC"
-  conf.cc.defines << "ESTALLOC_DEBUG"
+  conf.cc.defines << "PICORB_ALLOC_DEFAULT"
+#  conf.cc.defines << "PICORB_ALLOC_ESTALLOC"
+#  conf.cc.defines << "ESTALLOC_DEBUG"
   conf.cc.defines << "MRB_32BIT"
   conf.cc.defines << "USE_FAT_FLASH_DISK=1"
   conf.cc.defines << "FLASH_TARGET_OFFSET=0x00340000" # for 4MB flash
