b63178efc960e65ed60d07237b3a981d35d11ffc
HASUMI Hitoshi <hasumikin@gmail.com>
2025-04-11 17:07:27 +0900

    refactor

--- mrbgems/picoruby-net/mrblib/ntp.rb
+++ mrbgems/picoruby-net/mrblib/ntp.rb
@@ -2,7 +2,9 @@ class Net
   class NTP
     NTP_SERVER = "pool.ntp.org"
     NTP_PORT = 123
-    NTP_PACKET_SIZE = 48
+    # Seconds from 1900-01-01 to 1970-01-01
+    UNIX_TIME_BASE = 2_208_988_800 # (70*365+17)*24*60*60
+                                   #         ^^ leap years
 
     def self.get(ntp_server = NTP_SERVER, ntp_port = NTP_PORT)
       # Create a NTP packet
@@ -10,32 +12,18 @@ class Net
 
       r = UDPClient.send(ntp_server, ntp_port, ntp_packet, false)
 
-      if r&.size == NTP_PACKET_SIZE
-        # Extract NTP timestamp (seconds from 1900-01-01)
-        # @type var r: String
-        r40 = r[40]
-        r41 = r[41]
-        r42 = r[42]
-        r43 = r[43]
-        # @type var r40: String
-        # @type var r41: String
-        # @type var r42: String
-        # @type var r43: String
-        seconds = ( r40.ord << 24 | r41.ord << 16 | r42.ord << 8 | r43.ord ) - 2_208_988_800
-        # Extract NTP fraction (nanoseconds)
-        r44 = r[44]
-        r45 = r[45]
-        r46 = r[46]
-        r47 = r[47]
-        # @type var r44: String
-        # @type var r45: String
-        # @type var r46: String
-        # @type var r47: String
-        fraction = r44.ord << 24 | r45.ord << 16 | r46.ord << 8 | r47.ord
+      if r&.size == ntp_packet.size
+        # steep:ignore:start
+        # NTP timestamp
+        ntp_t = r[40].ord << 24 | r[41].ord << 16 | r[42].ord << 8 | r[43].ord
+        # NTP fraction (nanoseconds)
+        ntp_f = r[44].ord << 24 | r[45].ord << 16 | r[46].ord << 8 | r[47].ord
+        # steep:ignore:end
+        # Convert NTP timestamp to UNIX timestamp
+        timestamp = ntp_t - UNIX_TIME_BASE
         # Calculate nanoseconds
-        nanoseconds = (fraction * 1_000_000_000) >> 32
-        return [seconds, nanoseconds]
-
+        nanoseconds = (ntp_f * 1_000_000_000) >> 32
+        return [timestamp, nanoseconds]
       else
         raise "Failed to retrieve time from NTP server."
       end
