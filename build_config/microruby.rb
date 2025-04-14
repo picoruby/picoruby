7f09e0cc765080679fd00455829fec65d5583ed4
HASUMI Hitoshi <hasumikin@gmail.com>
2025-04-14 21:22:56 +0900

    refactor build_config

--- build_config/microruby.rb
+++ build_config/microruby.rb
@@ -4,40 +4,19 @@ MRuby::Build.new do |conf|
   conf.cc.defines << "MRB_TICK_UNIT=4"
   conf.cc.defines << "MRB_TIMESLICE_TICK_COUNT=3"
 
+  conf.cc.defines << "PICORB_ALLOC_ALIGN=8"
+  conf.cc.defines << "PICORB_ALLOC_ESTALLOC"
+  conf.cc.defines << "ESTALLOC_DEBUG"
+
   conf.gem github: 'picoruby/mruby-compiler2'
   conf.gem github: 'picoruby/mruby-bin-mrbc2'
   conf.gem core: 'picoruby-bin-microruby'
-  conf.gem core: "picoruby-mruby"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-io"
-  conf.gem core: "picoruby-machine"
-  conf.gem core: "mruby-file-stat"
-
-  conf.gem core: "picoruby-json"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-string-ext"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-array-ext"
-
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-time"
-  conf.gem core: "picoruby-env"
-
-  conf.gem core: "picoruby-io-console"
-  conf.gem core: "picoruby-picorubyvm"
-  conf.gem core: "picoruby-sandbox"
-
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-objectspace"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-metaprog"
-
-  conf.gem core: "picoruby-shell"
-  conf.gem core: "picoruby-vim"
-  conf.gem core: "picoruby-picoline"
-  conf.gem core: "picoruby-yaml"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-dir"
-
-  conf.gem core: "picoruby-bin-r2p2"
-
-  conf.gem core: "picoruby-mbedtls"
-  conf.gem core: "picoruby-net"
-  conf.gem core: "picoruby-base16"
-  conf.gem core: "picoruby-base64"
+  conf.gem core: 'picoruby-net'
+  conf.gem core: 'picoruby-mbedtls'
+  conf.gembox "stdlib-microruby"
+  conf.gembox "posix-microruby"
+  conf.gembox "r2p2"
+  conf.gembox "utils"
 
   conf.microruby
 end
--- build_config/r2p2_w-microruby-cortex-m33.rb
+++ build_config/r2p2_w-microruby-cortex-m33.rb
@@ -10,10 +10,10 @@ MRuby::CrossBuild.new("r2p2_w-microruby-cortex-m33") do |conf|
   conf.cc.defines << "MRB_TICK_UNIT=1"
   conf.cc.defines << "MRB_TIMESLICE_TICK_COUNT=10"
 
+  conf.cc.defines << "MRB_INT64"
+  conf.cc.defines << "MRB_32BIT"
   conf.cc.defines << "PICORB_ALLOC_ESTALLOC"
   conf.cc.defines << "ESTALLOC_DEBUG"
-  conf.cc.defines << "MRB_32BIT"
-  conf.cc.defines << "MRB_INT64"
   conf.cc.defines << "USE_FAT_FLASH_DISK=1"
   conf.cc.defines << "USE_WIFI"
 
@@ -37,14 +37,11 @@ MRuby::CrossBuild.new("r2p2_w-microruby-cortex-m33") do |conf|
   conf.cc.flags << "-Wall"
   conf.cc.flags << "-Wno-format"
   conf.cc.flags << "-Wno-unused-function"
-  conf.cc.flags << "-Wno-maybe-uninitialized"
   conf.cc.flags << "-ffunction-sections"
   conf.cc.flags << "-fdata-sections"
 
   conf.gem github: 'picoruby/mruby-compiler2'
   conf.gem core: "picoruby-mruby"
-  #conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-io"
-  #conf.gem core: "mruby-file-stat"
   conf.gem core: "picoruby-json"
   conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-string-ext"
   conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-array-ext"
@@ -58,27 +55,74 @@ MRuby::CrossBuild.new("r2p2_w-microruby-cortex-m33") do |conf|
   conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-error"
   conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-sprintf"
   conf.gem core: "picoruby-shell"
-  #conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-dir"
-
   conf.gem core: "picoruby-vfs"
   conf.gem core: "picoruby-filesystem-fat"
-
   conf.gem core: "picoruby-watchdog"
   conf.gem core: "picoruby-rng"
-
   conf.gem core: "picoruby-gpio"
   conf.gem core: "picoruby-adc"
   conf.gem core: "picoruby-i2c"
   conf.gem core: "picoruby-pwm"
   conf.gem core: "picoruby-spi"
   conf.gem core: "picoruby-uart"
-
   conf.gem core: "picoruby-mbedtls"
   conf.gem core: "picoruby-net"
   conf.gem core: "picoruby-base16"
   conf.gem core: "picoruby-base64"
   conf.gem core: "picoruby-ble"
   conf.gem core: "picoruby-yaml"
+  conf.gem core: "picoruby-vim"
+  conf.gem core: "picoruby-rng"
 
+#  # stdlib-microruby
+##  conf.gembox "stdlib-microruby"
+#  conf.gem github: 'picoruby/mruby-compiler2'
+#  conf.gem core: 'picoruby-machine'
+#  conf.gem core: "picoruby-mruby"
+#  conf.gem core: "picoruby-base16"
+#  conf.gem core: "picoruby-base64"
+#  conf.gem core: "picoruby-json"
+#  conf.gem core: "picoruby-yaml"
+#  conf.gem core: "picoruby-picorubyvm"
+#  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-string-ext"
+#  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-array-ext"
+#  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-time"
+#  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-objectspace"
+#  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-metaprog"
+#  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-error"
+#  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-sprintf"
+#
+#  # baremetal
+##  conf.gembox "baremetal"
+#  conf.gem core: "picoruby-vfs"
+#  conf.gem core: "picoruby-filesystem-fat"
+#  conf.gem core: "picoruby-watchdog"
+#
+#  # peripherals
+##  conf.gembox "peripherals"
+#  conf.gem core: "picoruby-gpio"
+#  conf.gem core: "picoruby-i2c"
+#  conf.gem core: "picoruby-spi"
+#  conf.gem core: "picoruby-adc"
+#  conf.gem core: "picoruby-uart"
+#  conf.gem core: "picoruby-pwm"
+#
+#  # r2p2
+##  conf.gembox "r2p2"
+#  conf.gem core: "picoruby-shell"
+#
+#  # cyw43
+##  conf.gembox "cyw43"
+#  conf.gem core: "picoruby-mbedtls"
+#  conf.gem core: "picoruby-net"
+#  conf.gem core: "picoruby-ble"
+#
+#  conf.gem core: "picoruby-env"
+#  conf.gem core: "picoruby-io-console"
+#  conf.gem core: "picoruby-sandbox"
+#  conf.gem core: "picoruby-rng"
+
+##  conf.gembox "peripheral_utils"
+##  conf.gembox "utils"
   conf.microruby
 end
