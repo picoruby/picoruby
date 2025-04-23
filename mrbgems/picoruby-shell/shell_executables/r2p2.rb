bd18e1cb290ec741b716a0acb9c13f7074ceb09e
HASUMI Hitoshi <hasumikin@gmail.com>
2025-04-23 11:47:46 +0900

    Fix to correspond to R2P2: https://github.com/picoruby/R2P2/commit/580b2cf85d46a4675df679a2467569c82e6d8ce9

--- mrbgems/picoruby-shell/shell_executables/r2p2.rb
+++ mrbgems/picoruby-shell/shell_executables/r2p2.rb
@@ -1,7 +1,8 @@
+require 'gpio'
+require 'cyw43'
 
 begin
   require "cyw43"
-  puts 1
   if CYW43.respond_to?(:enable_sta_mode)
     ENV['WIFI_MODULE'] = "cwy43"
     pin = GPIO.new(22, GPIO::IN|GPIO::PULL_UP)
