2054f0a6fd5f68a98701561bdb8881671a0ed751
HASUMI Hitoshi <hasumikin@gmail.com>
2025-05-04 19:38:44 +0900

    remove Time hack

--- mrbgems/picoruby-machine/mrblib/machine.rb
+++ mrbgems/picoruby-machine/mrblib/machine.rb
@@ -1,10 +1,2 @@
-if RUBY_ENGINE == "mruby"
-  class Time
-    def self.now(in: nil)
-      ts = Machine.get_hwclock
-      tv_sec = ts[0]
-      tv_nsec = ts[1].to_f / 1_000_000_000
-      Time.at(tv_sec + tv_nsec)
-    end
-  end
+class Machine
 end
