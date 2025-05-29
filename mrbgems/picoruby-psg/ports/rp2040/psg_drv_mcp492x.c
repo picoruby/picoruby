4b8bbe712ace749c30b40109a58bd338e97ea02d
HASUMI Hitoshi <hasumikin@gmail.com>
2025-05-30 08:31:08 +0900

    WIP

--- mrbgems/picoruby-psg/ports/rp2040/psg_drv_mcp492x.c
+++ mrbgems/picoruby-psg/ports/rp2040/psg_drv_mcp492x.c
@@ -8,16 +8,15 @@
 #define DAC_SPI1      spi1
 #define DAC_SPI_BAUD  10000000   /* 10 MHz */
 
-/* 2-sample DMA buffer */
-static uint16_t dac_buf[2];
-static int      dac_dma_ch;
-
+static spi_inst_t *spi_unit;
 static uint ldac_pin;
+static uint cs_pin;
 
 static void
 psg_mcp492x_init(uint8_t copi, uint8_t sck, uint8_t cs, uint8_t ldac)
 {
-  spi_inst_t *spi_unit;
+  ldac_pin = ldac;
+  cs_pin = cs;
   switch (copi) {
     case 3:
     case 7:
@@ -36,51 +35,48 @@ psg_mcp492x_init(uint8_t copi, uint8_t sck, uint8_t cs, uint8_t ldac)
   spi_init(spi_unit, DAC_SPI_BAUD);
   gpio_set_function(copi, GPIO_FUNC_SPI);
   gpio_set_function(sck,  GPIO_FUNC_SPI);
-  gpio_init(cs);
-  gpio_set_dir(cs, GPIO_OUT);
-  gpio_put(cs, 1);
-  gpio_init(ldac);
-  gpio_set_dir(ldac, GPIO_OUT);
-  ldac_pin = ldac;
-
-  dac_dma_ch = dma_claim_unused_channel(true);
-  dma_channel_config c = dma_channel_get_default_config(dac_dma_ch);
-  channel_config_set_transfer_data_size(&c, DMA_SIZE_16);
-  channel_config_set_dreq(&c, spi_get_dreq(spi_unit, true));
-  channel_config_set_read_increment(&c, true);
-  channel_config_set_write_increment(&c, false);
-  dma_channel_configure(dac_dma_ch, &c, &spi_get_hw(spi_unit)->dr, dac_buf, 2, false);
+  spi_set_format(spi_unit, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+  gpio_init(cs_pin);
+  gpio_set_dir(cs_pin, GPIO_OUT);
+  gpio_put(cs_pin, 1);
+  gpio_init(ldac_pin);
+  gpio_set_dir(ldac_pin, GPIO_OUT);
+  gpio_put(ldac_pin, 0);
 }
 
 void
 psg_mcp492x_start(void)
 {
-  gpio_put(ldac_pin, 0);
 }
 
 void
 psg_mcp492x_stop(void)
 {
-  gpio_put(ldac_pin, 1);
 }
 
 static inline void
 psg_mcp4922_write(uint16_t l, uint16_t r)
 {
-  while (dma_channel_is_busy(dac_dma_ch));
-  // build 16-bit frame: |C1|BUF|GA|SHDN|D11..0|
-  dac_buf[0] = 0x3000 | (l & 0x0FFF);  // Channel A
-  dac_buf[1] = 0xB000 | (r & 0x0FFF);  // Channel B
-  dma_channel_set_read_addr(dac_dma_ch, dac_buf, true);
+  uint16_t data[2];
+  data[0] = 0x3000 | (l & 0x0FFF);  // Channel A
+  data[1] = 0xB000 | (r & 0x0FFF);  // Channel B
+  gpio_put(ldac_pin, 1); // set LDAC high to prepare for transfer
+  gpio_put(cs_pin, 0);   // set CS low to start transfer
+  spi_write16_blocking(spi_unit, data, 2); // write stereo values
+  gpio_put(cs_pin, 1);   // set CS high to end transfer
+  gpio_put(ldac_pin, 0); // set LDAC low to latch value
 }
 
 static inline void
 psg_mcp4921_write(uint16_t l, uint16_t r)
 {
-  while (dma_channel_is_busy(dac_dma_ch));
-  uint16_t mono = (l + r) / 2;
-  dac_buf[0] = 0x3000 | (mono & 0x0FFF);  // Channel A
-  dma_channel_set_read_addr(dac_dma_ch, dac_buf, true);
+  uint16_t mono = (uint16_t)(((uint32_t)l + (uint32_t)r) / 2);
+  uint16_t data = 0x3000 | (mono & 0x0FFF);  // Channel A
+  gpio_put(ldac_pin, 1); // set LDAC high to prepare for transfer
+  gpio_put(cs_pin, 0);   // set CS low to start transfer
+  spi_write16_blocking(spi_unit, &data, 1); // write mono value
+  gpio_put(cs_pin, 1);   // set CS high to end transfer
+  gpio_put(ldac_pin, 0); // set LDAC low to latch value
 }
 
 const psg_output_api_t psg_drv_mcp4922 = {
