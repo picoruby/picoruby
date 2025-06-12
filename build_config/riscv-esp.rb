831d1327030306529da4291480034db51460fc49
yuuu <okazaki@fusic.co.jp>
2025-06-13 05:26:14 +0900

    Add Vim for ESP32.

--- build_config/riscv-esp.rb
+++ build_config/riscv-esp.rb
@@ -31,6 +31,7 @@ MRuby::CrossBuild.new("esp32") do |conf|
   conf.gem core: "picoruby-rmt"
   conf.gem core: "picoruby-adafruit_sk6812"
   conf.gem core: "picoruby-yaml"
+  conf.gem core: "picoruby-vim"
   conf.gem core: "picoruby-picoline"
   conf.gem core: "picoruby-base64"
   conf.gem core: "picoruby-mbedtls"
