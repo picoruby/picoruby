92873a69f5b3130d25aad6a135d3a1f85fdca2c9
Miyuki Koshiba <ksbmyk@gmail.com>
2025-06-14 19:39:15 +0900

    Split show method into show_rgb and show_hex

--- mrbgems/picoruby-adafruit_ws2812/mrblib/ws2812.rb
+++ mrbgems/picoruby-adafruit_ws2812/mrblib/ws2812.rb
@@ -11,23 +11,25 @@ class WS2812
       reset_ns: 60000)
   end
 
-  # ex. show(0xff0000, 0x00ff00, 0x0000ff)  # Hexadecimal RGB values
-  # or show([255, 0, 0], [0, 255, 0], [0, 0, 255]) # Array of RGB values
-  def show(*colors)
+  # ex. show([255, 0, 0], [0, 255, 0], [0, 0, 255]) # Array of RGB values
+  def show_rgb(*colors)
     bytes = []
     colors.each do |color|
-      r, g, b = parse_color(color)
+      r, g, b = color
       bytes << g << r << b
     end
 
     @rmt.write(bytes)
   end
 
-  def parse_color(color)
-    if color.is_a?(Integer)
-      [(color>>16)&0xFF, (color>>8)&0xFF, color&0xFF]
-    else
-      color
+  # ex. show(0xff0000, 0x00ff00, 0x0000ff)  # Hexadecimal RGB values
+  def show_hex(*colors)
+    bytes = []
+    colors.each do |color|
+      r, g, b = [(color>>16)&0xFF, (color>>8)&0xFF, color&0xFF]
+      bytes << g << r << b
     end
+
+    @rmt.write(bytes)
   end
 end
