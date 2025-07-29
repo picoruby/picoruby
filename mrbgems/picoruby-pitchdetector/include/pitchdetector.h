9c678b9be66f9fb22130165c4d7f8597ae50906d
HASUMI Hitoshi <hasumikin@gmail.com>
2025-07-29 12:21:27 +0900

    Revert "refactor"
    This reverts commit e609a34c0ca4f94f4da9ba1bfb83f7580b3eb33f.

--- mrbgems/picoruby-pitchdetector/include/pitchdetector.h
+++ mrbgems/picoruby-pitchdetector/include/pitchdetector.h
@@ -10,33 +10,9 @@ extern "C" {
 #define SAMPLE_RATE 8000 // 8kHz
 #define BUFFER_SIZE 1024
 
-// YIN algorithm constants
-#define YIN_THRESHOLD_BASE 0.15f
-#define YIN_THRESHOLD_MIN 0.10f
-#define YIN_THRESHOLD_MAX 0.25f
-#define MIN_PERIOD (SAMPLE_RATE / 800)  // 800Hz max
-#define MAX_PERIOD (BUFFER_SIZE / 2)    // Nyquist limit
-#define LOW_FREQ_THRESHOLD 120.0f       // Below this use extended analysis
-#define POWER_THRESHOLD 0.01f
-
-// Adaptive filtering constants
-#define SIGNAL_HISTORY_SIZE 8
-#define SNR_THRESHOLD 3.0f
-
-// Harmonics detection constants
-#define MAX_HARMONICS 4
-#define HARMONIC_TOLERANCE 0.02f  // 2% tolerance for harmonic detection
-#define FUNDAMENTAL_CONFIDENCE_THRESHOLD 0.7f
-
-// Platform-specific functions (DMA/hardware layer)
 void PITCHDETECTOR_start(uint8_t input);
 void PITCHDETECTOR_stop(void);
 float PITCHDETECTOR_detect_pitch(void);
-
-// Platform-independent functions (signal processing layer)
-void PITCHDETECTOR_init_processing(void);
-float PITCHDETECTOR_process_buffer(uint16_t *buffer, int size);
-float PITCHDETECTOR_calculate_signal_power(uint16_t *buffer, int size, float *dc_offset);
 void PITCHDETECTOR_set_volume_threshold(uint16_t value);
 
 #ifdef __cplusplus
