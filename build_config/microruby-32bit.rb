ef544d4f87f57a68b3f6c7f5f305b9965c553574
HASUMI Hitoshi <hasumikin@gmail.com>
2025-08-17 21:12:35 +0900

    Fix build_config to align with miroruby.rb (64-bit host)

--- build_config/microruby-32bit.rb
+++ build_config/microruby-32bit.rb
@@ -1,15 +1,19 @@
 MRuby::CrossBuild.new('microruby-32bit') do |conf|
   conf.toolchain :gcc
 
+  conf.cc.defines << "PICORB_PLATFORM_POSIX"
   conf.cc.defines << "MRB_TICK_UNIT=4"
   conf.cc.defines << "MRB_TIMESLICE_TICK_COUNT=3"
-  conf.cc.defines << "PICORB_PLATFORM_POSIX"
 
-  #conf.cc.defines << "PICORB_ALLOC_TLSF"
+  conf.cc.defines << "PICORB_ALLOC_ALIGN=4"
   conf.cc.defines << "PICORB_ALLOC_ESTALLOC"
+  conf.cc.defines << "ESTALLOC_DEBUG"
+
+  conf.cc.defines << "MRB_INT64"
   conf.cc.defines << "MRB_32BIT"
 
   conf.cc.flags << '-m32'
+  conf.cc.flags << '-static'
   conf.cc.flags << '-Wall'
   conf.cc.flags << '-Wextra'
   conf.cc.flags << '-Werror=address-of-packed-member'
@@ -24,33 +28,16 @@ MRuby::CrossBuild.new('microruby-32bit') do |conf|
   conf.gem core: 'mruby-compiler2'
   conf.gem core: 'mruby-bin-mrbc2'
   conf.gem core: 'picoruby-bin-microruby'
-  conf.gem core: "picoruby-mruby"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-io"
-  conf.gem core: "picoruby-machine"
-  conf.gem core: "mruby-file-stat"
+  #conf.gem core: 'picoruby-net'
+  #conf.gem core: 'picoruby-mbedtls'
+  conf.gem core: 'picoruby-require'
+  conf.gem core: 'picoruby-picotest'
+  conf.gembox "stdlib-microruby"
+  conf.gembox "posix-microruby"
+  conf.gembox "r2p2"
+  conf.gembox "utils"
 
-  conf.gem core: "picoruby-json"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-string-ext"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-array-ext"
-
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-time"
-  conf.gem core: "picoruby-env"
-
-  conf.gem core: "picoruby-io-console"
-  conf.gem core: "picoruby-picorubyvm"
-  conf.gem core: "picoruby-sandbox"
-
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-objectspace"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-metaprog"
-
-  conf.gem core: "picoruby-shell"
-  conf.gem core: "picoruby-vim"
-  conf.gem core: "picoruby-picoline"
-  conf.gem core: "picoruby-yaml"
-  conf.gem gemdir: "mrbgems/picoruby-mruby/lib/mruby/mrbgems/mruby-dir"
-
-  conf.gem core: "picoruby-bin-r2p2"
+  #conf.gem  core: "picoruby-rapicco"
 
   conf.microruby
 end
-
