31a46a04b362914878d21616795cf68e17427916
HASUMI Hitoshi <hasumikin@gmail.com>
2025-10-02 01:00:43 +0900

    Move mbedtls_hardware_poll() from picoruby-net to picoruby-mbedtls

--- mrbgems/picoruby-net/ports/posix/net.c
+++ mrbgems/picoruby-net/ports/posix/net.c
@@ -1,10 +1,4 @@
 #include <unistd.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <errno.h>
 
 
 void
@@ -23,28 +17,4 @@ void
 lwip_end(void)
 {
   // no-op
-}
-
-int
-mbedtls_hardware_poll(void *data __attribute__((unused)), unsigned char *output, size_t len, size_t *olen)
-{
-  int fd = open("/dev/urandom", O_RDONLY);
-  if (fd == -1) {
-    return -1;  // Error opening /dev/urandom
-  }
-
-  ssize_t bytes_read = read(fd, output, len);
-  if (bytes_read < 0) {
-    close(fd);
-    return -1;  // Error reading from /dev/urandom
-  }
-
-  close(fd);
-
-  // Set the number of bytes written to the output
-  if (olen != NULL) {
-    *olen = bytes_read;
-  }
-
-  return 0;  // Success
 }
