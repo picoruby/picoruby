b2f73f8752a3b229cae9eb02fcc745d51155efa7
HASUMI Hitoshi <hasumikin@gmail.com>
2025-10-29 10:48:19 +0900

    use mruby/c's allocator for POSIX build

--- build_config/default.rb
+++ build_config/default.rb
@@ -7,7 +7,7 @@ MRuby::Build.new do |conf|
   conf.cc.defines << "MRBC_TIMESLICE_TICK_COUNT=3"
 
   conf.posix
-  conf.picoruby(alloc_libc: true)
+  conf.picoruby(alloc_libc: false)
 
   conf.gembox "minimum"
   conf.gembox "core"
