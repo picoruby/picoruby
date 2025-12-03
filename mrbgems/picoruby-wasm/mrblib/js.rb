999d1cef9129d01564ec0dd25026b486f030f772
HASUMI Hitoshi <hasumikin@gmail.com>
2025-12-03 16:08:01 +0900

    Improve JS infrastructure
    - DOM API: createElement, createTextNode, appendChild, removeChild, replaceChild, insertBefore, setAttribute, removeAttribute
    - Event handler: removeEventListener, preventDefault, stopPropagation
    - Available types in property setter: String, Integer, Float, Boolean, nil, JS::Object
    - Test: test_dom_apis.html

--- mrbgems/picoruby-wasm/mrblib/js.rb
+++ mrbgems/picoruby-wasm/mrblib/js.rb
@@ -15,13 +15,19 @@ module JS
       callback_id = block.object_id
       _add_event_listener(callback_id, event_type)
       CALLBACKS[callback_id] = block
-      return callback_id # When removeEventListener, we need to pass this id
+      callback_id
+    end
+
+    def removeEventListener(callback_id)
+      return false unless callback_id
+      result = _removeEventListener(callback_id)
+      CALLBACKS.delete(callback_id) if result
+      result
     end
 
     def fetch(url, &block)
       callback_id = block.object_id
       _fetch_and_suspend(url, callback_id)
-      # resumed by calback
       block.call($promise_responses[callback_id])
       $promise_responses.delete(callback_id)
     end
@@ -29,10 +35,17 @@ module JS
     def to_binary
       callback_id = self.object_id
       _to_binary_and_suspend(callback_id)
-      # resumed by calback
       result = $promise_responses[callback_id]
       $promise_responses.delete(callback_id)
       result.to_s
+    end
+
+    def preventDefault
+      self[:preventDefault].call(self)
+    end
+
+    def stopPropagation
+      self[:stopPropagation].call(self)
     end
   end
 end
