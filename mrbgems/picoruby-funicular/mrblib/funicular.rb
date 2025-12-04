679f36b73ca7a34b7d21315858cf6ff777ee8486
HASUMI Hitoshi <hasumikin@gmail.com>
2025-12-04 15:11:45 +0900

    Funicular::Component
    - Basic implementation with lifecycle hooks
    - DSL to make HTML
    - Pipeline for VDOM integration
    - `Funicular.start`

--- mrbgems/picoruby-funicular/mrblib/funicular.rb
+++ mrbgems/picoruby-funicular/mrblib/funicular.rb
@@ -6,4 +6,24 @@ module Funicular
   def self.version
     VERSION
   end
+
+  # Start Funicular application
+  # Usage:
+  #   Funicular.start(MyComponent, container: 'app')
+  #   Funicular.start(MyComponent, container: 'app', props: { name: 'John' })
+  def self.start(component_class, container: 'app', props: {})
+    container_element = if container.is_a?(String)
+      JS.document.getElementById(container)
+    else
+      container
+    end
+
+    unless container_element
+      raise "Container element not found: #{container}"
+    end
+
+    instance = component_class.new(props)
+    instance.mount(container_element)
+    instance
+  end
 end
