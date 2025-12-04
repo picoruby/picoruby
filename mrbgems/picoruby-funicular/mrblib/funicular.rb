0c2903bbfd1e4bcbbfa81a1e1d3c23af4808c47d
HASUMI Hitoshi <hasumikin@gmail.com>
2025-12-04 16:17:10 +0900

    Funicular::Router
    - Simple router with hash base routing
      - No nested resource, no history yet
    - Fix JS.object impelmenting aref and recactoring common code

--- mrbgems/picoruby-funicular/mrblib/funicular.rb
+++ mrbgems/picoruby-funicular/mrblib/funicular.rb
@@ -7,11 +7,17 @@ module Funicular
     VERSION
   end
 
+  @router = nil
+
+  def self.router
+    @router
+  end
+
   # Start Funicular application
   # Usage:
   #   Funicular.start(MyComponent, container: 'app')
   #   Funicular.start(MyComponent, container: 'app', props: { name: 'John' })
-  def self.start(component_class, container: 'app', props: {})
+  def self.start(component_class = nil, container: 'app', props: {}, &block)
     container_element = if container.is_a?(String)
       JS.document.getElementById(container)
     else
@@ -22,8 +28,22 @@ module Funicular
       raise "Container element not found: #{container}"
     end
 
-    instance = component_class.new(props)
-    instance.mount(container_element)
-    instance
+    # If block is given, use router mode
+    if block
+      router = Router.new(container_element)
+      @router = router
+      block.call(router)
+      router.start
+      return router
+    end
+
+    # Otherwise, mount single component (backward compatible)
+    if component_class
+      instance = component_class.new(props)
+      instance.mount(container_element)
+      return instance
+    end
+
+    raise "Either component_class or block must be provided"
   end
 end
