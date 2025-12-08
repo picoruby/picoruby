25ad1340113dfeb04e3981d920d93bb1adbdeb50
HASUMI Hitoshi <hasumikin@gmail.com>
2025-12-08 17:23:41 +0900

    Fix Logout

--- mrbgems/picoruby-funicular/demo/rails_chat/app/controllers/sessions_controller.rb
+++ mrbgems/picoruby-funicular/demo/rails_chat/app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create, :destroy]
 
   def create
     user = User.find_by(username: params[:username])
