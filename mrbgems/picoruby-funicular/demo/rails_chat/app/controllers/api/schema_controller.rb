59285ec8037d5e2f7ad229073650f8add9f31c2e
HASUMI Hitoshi <hasumikin@gmail.com>
2025-12-09 09:58:23 +0900

    Apply Funicular::Model's improvements regarding CRUD methods

--- mrbgems/picoruby-funicular/demo/rails_chat/app/controllers/api/schema_controller.rb
+++ mrbgems/picoruby-funicular/demo/rails_chat/app/controllers/api/schema_controller.rb
@@ -18,4 +18,32 @@ class Api::SchemaController < ApplicationController
       }
     }
   end
+
+  def session
+    render json: {
+      attributes: {
+        "username" => { type: "string", readonly: false },
+        "password" => { type: "string", readonly: false }
+      },
+      endpoints: {
+        "create" => { method: "POST", path: "/login" },
+        "destroy" => { method: "DELETE", path: "/logout" },
+        "current" => { method: "GET", path: "/current_user" }
+      }
+    }
+  end
+
+  def channel
+    render json: {
+      attributes: {
+        "id" => { type: "integer", readonly: true },
+        "name" => { type: "string", readonly: true },
+        "description" => { type: "string", readonly: true }
+      },
+      endpoints: {
+        "all" => { method: "GET", path: "/channels" },
+        "find" => { method: "GET", path: "/channels/:id" }
+      }
+    }
+  end
 end
