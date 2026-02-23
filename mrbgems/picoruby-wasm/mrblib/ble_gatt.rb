e687cbad717f40510ca14c82cceeabec026391f1
HASUMI Hitoshi <hasumikin@gmail.com>
2026-02-23 12:19:07 +0900

    WIP

--- mrbgems/picoruby-wasm/mrblib/ble_gatt.rb
+++ mrbgems/picoruby-wasm/mrblib/ble_gatt.rb
@@ -1,6 +1,7 @@
 module JS
   module BLE
     module GATT
+<<<<<<< HEAD
       # Convert UUID string to the format Web Bluetooth expects.
       # "0xffe0" -> Integer 0xffe0
       # "heart_rate" or "00001234-..." -> pass through as String
@@ -27,6 +28,22 @@ module JS
         navigator = JS.global[:navigator] # steep:ignore
         bluetooth = navigator[:bluetooth]
         raise "Web Bluetooth API not supported" unless bluetooth
+=======
+      # Request a BLE device via browser picker.
+      # Must be called from a user gesture handler (click, etc.).
+      #
+      # @param name [String, nil] exact device name filter
+      # @param name_prefix [String, nil] device name prefix filter
+      # @param services [Array<String>, nil] required service UUIDs
+      # @param optional_services [Array<String>, nil] optional service UUIDs
+      # @return [JS::BLE::GATT::Device]
+      def self.request_device(name: nil, name_prefix: nil,
+                              services: nil, optional_services: nil)
+        navigator = JS.global[:navigator]
+        raise "Web Bluetooth not available" unless navigator
+        bluetooth = navigator[:bluetooth]
+        raise "Web Bluetooth API not supported in this browser" unless bluetooth
+>>>>>>> f7cfe1d5 (WIP)
         options = JS.global.create_object
 
         if name || name_prefix || services
@@ -35,7 +52,11 @@ module JS
           filter[:name] = name if name
           filter[:namePrefix] = name_prefix if name_prefix
           if services
+<<<<<<< HEAD
             filter[:services] = uuids_to_js_array(services)
+=======
+            filter[:services] = JS::Bridge.to_js(services)
+>>>>>>> f7cfe1d5 (WIP)
           end
           filters.push(filter)
           options[:filters] = filters
@@ -44,11 +65,19 @@ module JS
         end
 
         if optional_services
+<<<<<<< HEAD
           options[:optionalServices] = uuids_to_js_array(optional_services)
         end
 
         device = nil
         bluetooth.requestDevice(options).then do |js_device| # steep:ignore
+=======
+          options[:optionalServices] = JS::Bridge.to_js(optional_services)
+        end
+
+        device = nil
+        bluetooth.requestDevice(options).then do |js_device|
+>>>>>>> f7cfe1d5 (WIP)
           device = Device.new(js_device)
         end
         device
@@ -65,11 +94,18 @@ module JS
         end
 
         # Connect to the GATT server on this device.
+<<<<<<< HEAD
         def connect
           # @type var gatt: JS::Object
           gatt = @js_device[:gatt] # steep:ignore
           server = nil
           gatt.connect.then do |js_server| # steep:ignore
+=======
+        # @return [JS::BLE::GATT::Server]
+        def connect
+          server = nil
+          @js_device[:gatt].connect.then do |js_server|
+>>>>>>> f7cfe1d5 (WIP)
             server = Server.new(js_server)
           end
           server
@@ -77,17 +113,25 @@ module JS
 
         # Disconnect from the GATT server.
         def disconnect
+<<<<<<< HEAD
           # @type var gatt: JS::Object
           gatt = @js_device[:gatt] # steep:ignore
           gatt.disconnect # steep:ignore
+=======
+          @js_device[:gatt].disconnect
+>>>>>>> f7cfe1d5 (WIP)
         end
 
         # Whether the device is currently connected.
         # @return [Boolean]
         def connected?
+<<<<<<< HEAD
           # @type var gatt: JS::Object
           gatt = @js_device[:gatt] # steep:ignore
           gatt[:connected] == true
+=======
+          @js_device[:gatt][:connected] == true
+>>>>>>> f7cfe1d5 (WIP)
         end
 
         # Register a callback for disconnection events.
@@ -107,9 +151,17 @@ module JS
         end
 
         # Get a primary service by UUID string.
+<<<<<<< HEAD
         def service(uuid)
           svc = nil
           @js_server.getPrimaryService(GATT.normalize_uuid(uuid)).then do |js_service| # steep:ignore
+=======
+        # @param uuid [String]
+        # @return [JS::BLE::GATT::Service]
+        def service(uuid)
+          svc = nil
+          @js_server.getPrimaryService(uuid).then do |js_service|
+>>>>>>> f7cfe1d5 (WIP)
             svc = Service.new(js_service)
           end
           svc
@@ -125,9 +177,17 @@ module JS
         end
 
         # Get a characteristic by UUID string.
+<<<<<<< HEAD
         def characteristic(uuid)
           char = nil
           @js_service.getCharacteristic(GATT.normalize_uuid(uuid)).then do |js_char| # steep:ignore
+=======
+        # @param uuid [String]
+        # @return [JS::BLE::GATT::Characteristic]
+        def characteristic(uuid)
+          char = nil
+          @js_service.getCharacteristic(uuid).then do |js_char|
+>>>>>>> f7cfe1d5 (WIP)
             char = Characteristic.new(js_char)
           end
           char
@@ -143,26 +203,48 @@ module JS
         end
 
         # Read the characteristic value.
+<<<<<<< HEAD
         def read
           data = nil
           @js_char.readValue.then do |js_dataview| # steep:ignore
+=======
+        # @return [String] binary string
+        def read
+          data = nil
+          @js_char.readValue.then do |js_dataview|
+>>>>>>> f7cfe1d5 (WIP)
             data = JS::BLE._dataview_to_string(js_dataview)
           end
           data
         end
 
         # Write data to the characteristic.
+<<<<<<< HEAD
         def write(data, without_response: false)
           js_uint8 = JS::BLE._string_to_uint8array(data)
           if without_response
             @js_char.writeValueWithoutResponse(js_uint8).then {} # steep:ignore
           else
             @js_char.writeValueWithResponse(js_uint8).then {} # steep:ignore
+=======
+        # @param data [String] binary string to write
+        # @param without_response [Boolean] use writeValueWithoutResponse
+        def write(data, without_response: false)
+          js_uint8 = JS::BLE._string_to_uint8array(data)
+          if without_response
+            @js_char.writeValueWithoutResponse(js_uint8).then {}
+          else
+            @js_char.writeValueWithResponse(js_uint8).then {}
+>>>>>>> f7cfe1d5 (WIP)
           end
         end
 
         # Register a notification callback.
         # The block receives binary String data on each notification.
+<<<<<<< HEAD
+=======
+        # @yield [String] binary notification data
+>>>>>>> f7cfe1d5 (WIP)
         def on_change(&block)
           callback_id = block.object_id
           JS::Object::CALLBACKS[callback_id] = block
@@ -171,12 +253,20 @@ module JS
 
         # Start receiving notifications.
         def start_notify
+<<<<<<< HEAD
           @js_char.startNotifications.then {} # steep:ignore
+=======
+          @js_char.startNotifications.then {}
+>>>>>>> f7cfe1d5 (WIP)
         end
 
         # Stop receiving notifications.
         def stop_notify
+<<<<<<< HEAD
           @js_char.stopNotifications.then {} # steep:ignore
+=======
+          @js_char.stopNotifications.then {}
+>>>>>>> f7cfe1d5 (WIP)
         end
       end
     end
--- mrbgems/picoruby-wasm/mrblib/ble_uart.rb
+++ mrbgems/picoruby-wasm/mrblib/ble_uart.rb
@@ -1,5 +1,6 @@
 module JS
   module BLE
+<<<<<<< HEAD
     # High-level IO-compatible wrapper for BLE UART communication.
     # Defaults to Nordic UART Service (NUS) UUIDs, but accepts custom UUIDs.
     # TX = write direction (Central -> Peripheral)
@@ -9,12 +10,22 @@ module JS
       NUS_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
       NUS_TX_CHAR_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
       NUS_RX_CHAR_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+=======
+    # High-level IO-compatible wrapper for Nordic UART Service (NUS).
+    # Provides blocking and non-blocking read/write with internal buffer.
+    class UART
+      # Nordic UART Service UUIDs
+      SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+      TX_CHAR_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+      RX_CHAR_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+>>>>>>> f7cfe1d5 (WIP)
 
       DEFAULT_TIMEOUT = nil
 
       attr_reader :device
 
       # Connect to a BLE UART device.
+<<<<<<< HEAD
       def initialize(service_uuid: NUS_SERVICE_UUID,
                      tx_uuid: NUS_TX_CHAR_UUID,
                      rx_uuid: NUS_RX_CHAR_UUID,
@@ -42,11 +53,40 @@ module JS
         @rx_char.start_notify # steep:ignore
 
         @device.on_disconnected do # steep:ignore
+=======
+      # Accepts same filter options as GATT.request_device.
+      # The NUS service UUID is automatically added to optional_services.
+      def initialize(name: nil, name_prefix: nil,
+                     services: nil, optional_services: nil)
+        opt = optional_services ? optional_services.dup : []
+        opt << SERVICE_UUID unless opt.include?(SERVICE_UUID)
+
+        @device = GATT.request_device(
+          name: name,
+          name_prefix: name_prefix,
+          services: services,
+          optional_services: opt
+        )
+
+        @buffer = ""
+        @server = @device.connect
+        @service = @server.service(SERVICE_UUID)
+        @tx_char = @service.characteristic(TX_CHAR_UUID)
+        @rx_char = @service.characteristic(RX_CHAR_UUID)
+
+        @rx_char.on_change do |data|
+          @buffer << data
+        end
+        @rx_char.start_notify
+
+        @device.on_disconnected do
+>>>>>>> f7cfe1d5 (WIP)
           @connected = false
         end
         @connected = true
       end
 
+<<<<<<< HEAD
       # Write string data to the TX characteristic.
       def write(data)
         @tx_char.write(data, without_response: true) # steep:ignore
@@ -54,24 +94,50 @@ module JS
       end
 
       # Write string data with trailing newline.
+=======
+      # Write string data to the UART TX characteristic.
+      # @param data [String]
+      # @return [Integer] number of bytes written
+      def write(data)
+        @tx_char.write(data, without_response: true)
+        data.length
+      end
+
+      # Write string data with trailing newline.
+      # @param data [String]
+>>>>>>> f7cfe1d5 (WIP)
       def puts(data)
         write(data + "\n")
         nil
       end
 
       # Read up to nbytes bytes. Blocks until enough data or timeout.
+<<<<<<< HEAD
       def read(nbytes, timeout: DEFAULT_TIMEOUT)
         deadline = timeout ? Time.now.to_f + timeout : nil
         while @buffer.bytesize < nbytes
+=======
+      # @param nbytes [Integer]
+      # @param timeout [Integer, nil] timeout in seconds (nil = block forever)
+      # @return [String, nil] nil on timeout
+      def read(nbytes, timeout: DEFAULT_TIMEOUT)
+        deadline = timeout ? Time.now.to_f + timeout : nil
+        while @buffer.length < nbytes
+>>>>>>> f7cfe1d5 (WIP)
           return nil if deadline && Time.now.to_f >= deadline
           sleep 0.05
         end
         result = @buffer.byteslice(0, nbytes)
+<<<<<<< HEAD
         @buffer = @buffer.byteslice(nbytes, @buffer.bytesize - nbytes) || ""
+=======
+        @buffer = @buffer.byteslice(nbytes, @buffer.length - nbytes) || ""
+>>>>>>> f7cfe1d5 (WIP)
         result
       end
 
       # Read a line (blocks until newline or timeout).
+<<<<<<< HEAD
       def gets(timeout: DEFAULT_TIMEOUT)
         deadline = timeout ? Time.now.to_f + timeout : nil
         while true
@@ -79,6 +145,17 @@ module JS
           if idx
             line = @buffer.byteslice(0, idx + 1)
             @buffer = @buffer.byteslice(idx + 1, @buffer.bytesize - idx - 1) || ""
+=======
+      # @param timeout [Integer, nil] timeout in seconds
+      # @return [String, nil] nil on timeout
+      def gets(timeout: DEFAULT_TIMEOUT)
+        deadline = timeout ? Time.now.to_f + timeout : nil
+        loop do
+          idx = @buffer.index("\n")
+          if idx
+            line = @buffer.byteslice(0, idx + 1)
+            @buffer = @buffer.byteslice(idx + 1, @buffer.length - idx - 1) || ""
+>>>>>>> f7cfe1d5 (WIP)
             return line
           end
           return nil if deadline && Time.now.to_f >= deadline
@@ -87,15 +164,26 @@ module JS
       end
 
       # Non-blocking read: returns up to nbytes from buffer, or nil if empty.
+<<<<<<< HEAD
       def read_nonblock(nbytes)
         return nil if @buffer.empty?
         actual = nbytes < @buffer.bytesize ? nbytes : @buffer.bytesize
         result = @buffer.byteslice(0, actual)
         @buffer = @buffer.byteslice(actual, @buffer.bytesize - actual) || ""
+=======
+      # @param nbytes [Integer]
+      # @return [String, nil]
+      def read_nonblock(nbytes)
+        return nil if @buffer.empty?
+        actual = nbytes < @buffer.length ? nbytes : @buffer.length
+        result = @buffer.byteslice(0, actual)
+        @buffer = @buffer.byteslice(actual, @buffer.length - actual) || ""
+>>>>>>> f7cfe1d5 (WIP)
         result
       end
 
       # Number of bytes available in the receive buffer.
+<<<<<<< HEAD
       def available
         @buffer.bytesize
       end
@@ -107,12 +195,28 @@ module JS
       # Whether the device is currently connected.
       def connected?
         @connected && @device.connected? # steep:ignore
+=======
+      # @return [Integer]
+      def available
+        @buffer.length
+      end
+
+      # Whether the device is currently connected.
+      # @return [Boolean]
+      def connected?
+        @connected && @device.connected?
+>>>>>>> f7cfe1d5 (WIP)
       end
 
       # Disconnect and clean up.
       def close
+<<<<<<< HEAD
         @rx_char.stop_notify # steep:ignore
         @device.disconnect # steep:ignore
+=======
+        @rx_char.stop_notify
+        @device.disconnect
+>>>>>>> f7cfe1d5 (WIP)
         @connected = false
         @buffer = ""
       end
