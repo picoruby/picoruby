d53f94836320a5117f6f942f1bd86a144d51bf4b
HASUMI Hitoshi <hasumikin@gmail.com>
2026-02-24 16:04:48 +0900

    Fix ref count

--- mrbgems/picoruby-ble/src/mrubyc/ble.c
+++ mrbgems/picoruby-ble/src/mrubyc/ble.c
@@ -112,8 +112,9 @@ c_push_read_value(mrbc_vm *vm, mrbc_value *v, int argc)
   }
   mrbc_value handle= GET_ARG(1);
   mrbc_value read_value = GET_ARG(2);
+  mrbc_incref(&read_value);  // for hash ownership
   mrbc_hash_set(&read_values, &handle, &read_value);
-  mrbc_incref(&read_value);
+  mrbc_incref(&read_value);  // for return value
   SET_RETURN(read_value);
 }
 
