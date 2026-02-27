18b4354a59608a1d8d733f5dd6f93feb38de009f
HASUMI Hitoshi <hasumikin@gmail.com>
2026-02-27 15:18:43 +0900

    Fix build

--- mrbgems/picoruby-mruby/src/mruby.c
+++ mrbgems/picoruby-mruby/src/mruby.c
@@ -1,11 +1,15 @@
 #include "mruby.h"
 
+#if defined(PICORB_PLATFORM_POSIX)
 void mrb_file_ext_init(mrb_state *mrb);
+#endif
 
 void
 mrb_picoruby_mruby_gem_init(mrb_state* mrb)
 {
+#if defined(PICORB_PLATFORM_POSIX)
   mrb_file_ext_init(mrb);
+#endif
 }
 
 void
