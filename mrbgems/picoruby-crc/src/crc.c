32c11d2478f706b394184674d1f925e5d76897b2
HASUMI Hitoshi <hasumikin@gmail.com>
2026-03-12 17:02:04 +0900

    Implement CRC16

--- mrbgems/picoruby-crc/src/crc.c
+++ mrbgems/picoruby-crc/src/crc.c
@@ -18,6 +18,22 @@ generate_crc32(uint8_t *str, size_t len, uint32_t crc)
   return ~crc_value;
 }
 
+static uint16_t
+generate_crc16(uint8_t *str, size_t len, uint16_t crc)
+{
+  for (size_t i = 0; i < len; i++) {
+    crc ^= (uint16_t)str[i] << 8;
+    for (int j = 0; j < 8; j++) {
+      if (crc & 0x8000) {
+        crc = (crc << 1) ^ 0x1021;
+      } else {
+        crc <<= 1;
+      }
+    }
+  }
+  return crc & 0xFFFF;
+}
+
 #if defined(PICORB_VM_MRUBY)
 
 #include "mruby/crc.c"
