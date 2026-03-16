f21361d8b632aa23728086d7e51ec5b77a8a76d4
Yuhei Okazaki <yuuu@users.noreply.github.com>
2026-03-16 14:46:14 +0900

    Enable Wi-Fi auto reconnect on ESP32 (#343)
    * Enable Wi-Fi auto reconnect on ESP32
    
    * Network abstraction layer for different platforms
    
    (Still experimental)
    
    This module is supposed to be used as a namespace for network related classes and modules.
    
    Usage: require "network" instead of "cyw43"|"esp32" and use `Network::WiFi.init` instead of `CYW43.init` and `ESP32::WiFi.init`, for example.
    This idea will mitigate platform incompatibility, but it will not necessarily be perfect: the existing BLE module for Pico W should be tweaked.
    
    * Align ESP32::WiFi interface with CYW43
    
    * Changed the timeout to 10 seconds. The timeout was too short for the ESP32.
    
    * Use picoruby-network in wifi_connect.rb
    
    * Add the ConnectTimeout class to the Network module
    
    * Using picoruby-network to remove ENV[‘WIFI_MODULE’] and reduce the complexity of shell and R2P2.
    
    * Fixed typos in comments and argument names.
    
    * Since `Machine.mcu_name` is deprecated, use `RUBY_PLATFORM` instead.
    
    ---------
    
    Co-authored-by: HASUMI Hitoshi <hasumikin@gmail.com>

---
+++ mrbgems/picoruby-network/mrblib/network.rb
@@ -0,0 +1,24 @@
+require "machine"
+
+module Network
+  case RUBY_PLATFORM
+  when "RP2040", "RP2350"
+    begin
+      require "cyw43"
+      WiFi = CYW43
+      ConnectTimeout = CYW43::ConnectTimeout
+    rescue LoadError # Only LoadError should be rescued
+    end
+  when "ESP32"
+    begin
+      require "esp32"
+      WiFi = ESP32::WiFi
+      ConnectTimeout = ESP32::ConnectTimeout
+    rescue LoadError # Only LoadError should be rescued
+    end
+  when "POSIX"
+    # no-op. Calling Network:WiFi raises error
+  else
+    # Should not reach here
+  end
+end
