671b8b750c089404a78b282138cd60fc7da017b1
Yuhei Okazaki <okazaki@fusic.co.jp>
2026-03-28 07:21:38 +0900

    ESP32: Fix binary frame corruption by disabling VFS line ending conversion in raw mode

--- mrbgems/picoruby-io-console/ports/esp32/io-console.c
+++ mrbgems/picoruby-io-console/ports/esp32/io-console.c
@@ -1,6 +1,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "sdkconfig.h"
+#include "driver/uart_vfs.h"
+#if defined(CONFIG_ESP_CONSOLE_SECONDARY_USB_SERIAL_JTAG) || \
+    defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG)
+#include "driver/usb_serial_jtag_vfs.h"
+#endif
 
 /*-------------------------------------
  *
@@ -25,6 +31,16 @@ io_raw_bang(bool nonblock)
   (void)nonblock;
   raw_mode_saved = raw_mode;
   raw_mode = true;
+  /* Disable line ending conversion on both TX and RX so binary frames are not corrupted */
+  uart_vfs_dev_port_set_tx_line_endings(
+    CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_LF);
+  uart_vfs_dev_port_set_rx_line_endings(
+    CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_LF);
+#if defined(CONFIG_ESP_CONSOLE_SECONDARY_USB_SERIAL_JTAG) || \
+    defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG)
+  usb_serial_jtag_vfs_set_tx_line_endings(ESP_LINE_ENDINGS_LF);
+  usb_serial_jtag_vfs_set_rx_line_endings(ESP_LINE_ENDINGS_LF);
+#endif
 }
 
 void
@@ -32,6 +48,16 @@ io_cooked_bang(void)
 {
   raw_mode_saved = raw_mode;
   raw_mode = false;
+  /* Restore line ending conversion for normal terminal I/O */
+  uart_vfs_dev_port_set_tx_line_endings(
+    CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CRLF);
+  uart_vfs_dev_port_set_rx_line_endings(
+    CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CR);
+#if defined(CONFIG_ESP_CONSOLE_SECONDARY_USB_SERIAL_JTAG) || \
+    defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG)
+  usb_serial_jtag_vfs_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+  usb_serial_jtag_vfs_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+#endif
 }
 
 void
@@ -52,4 +78,15 @@ io__restore_termios(void)
 {
   raw_mode = raw_mode_saved;
   echo_mode = echo_mode_saved;
+  if (!raw_mode) {
+    uart_vfs_dev_port_set_tx_line_endings(
+      CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CRLF);
+    uart_vfs_dev_port_set_rx_line_endings(
+      CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CR);
+#if defined(CONFIG_ESP_CONSOLE_SECONDARY_USB_SERIAL_JTAG) || \
+    defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG)
+    usb_serial_jtag_vfs_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+    usb_serial_jtag_vfs_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+#endif
+  }
 }
