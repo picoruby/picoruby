5ab8a2cc22d2e5beffdcbdb1528e1b3727073f36
HASUMI Hitoshi <hasumikin@gmail.com>
2026-03-30 22:38:23 +0900

    Fix test build

--- build_config/picoruby-test.rb
+++ build_config/picoruby-test.rb
@@ -6,7 +6,9 @@ MRuby::Build.new do |conf|
   conf.cc.defines << "MRB_TIMESLICE_TICK_COUNT=3"
   conf.cc.defines << "PICORB_ALLOC_ALIGN=8"
   conf.cc.defines << "PICORB_ALLOC_ESTALLOC"
-  conf.cc.defines << "ESTALLOC_DEBUG"
+  conf.cc.defines << "PICORB_DEBUG"
+  conf.cc.defines << "MRB_INT64"
+  conf.cc.defines << "MRB_NO_BOXING"
   conf.cc.defines << "MRB_UTF8_STRING"
 
   conf.picoruby
