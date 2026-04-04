04587bbe8edc8f53df97e6e65228973c174982db
HASUMI Hitoshi <hasumikin@gmail.com>
2026-04-04 09:35:09 +0900

    version 3.4.3 for only npm (picoruby.wasm and picorbc.wasm)

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
 #if !defined(__VERSION_H__)
 #define __VERSION_H__
 
-#define PICORUBY_VERSION "3.4.2"
+#define PICORUBY_VERSION "3.4.3"
 
 const char* picorb_version(void);
 const char* picorb_version_with_build_info(void);
