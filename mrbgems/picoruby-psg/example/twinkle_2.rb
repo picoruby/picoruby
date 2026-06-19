cda8f02d73e7999da22f83bf8995fb8f7bb998d4
HASUMI Hitoshi <hasumikin@gmail.com>
2026-06-19 15:45:07 +0900

    add require "psg"

--- mrbgems/picoruby-psg/example/twinkle_2.rb
+++ mrbgems/picoruby-psg/example/twinkle_2.rb
@@ -1,3 +1,5 @@
+require 'psg'
+
 tracks = [
   '@0 T120 S0 M800 L4 O5 f  f >c   c |  d    d   c2    |<b-  b-  a  a | g  g f2|>c  c <b- b-| a  a  g2     |>c   c  <b- b- | a  a g2        |f  f> c   c |  d    d   c2    | <b- b-  a  a | g  g f2',
   '@0 T120 S0      L4 O4 a >c  f   f |  f    f   g   f | e   c   f  c |<b- b-a2|>a  a  g  g | f  f  e2     | g   f   d  e  | c  f e8d8c8<b-8|a >c  f   f |  f    f   g   f |  e  c   f  c |<b- b-a2',
