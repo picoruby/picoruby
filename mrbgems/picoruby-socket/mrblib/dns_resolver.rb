63dbce00c305ccf88982828d44bf256091bd29bc
HASUMI Hitoshi <hasumikin@gmail.com>
2026-07-17 08:52:28 +0900

    Improve DNS resolve

---
+++ mrbgems/picoruby-socket/mrblib/dns_resolver.rb
@@ -0,0 +1,22 @@
+if Object.const_defined?(:SocketDNSResolver)
+  class SocketDNSResolver
+    def self.resolve(host)
+      new(host).resolve(host)
+    end
+
+    def resolve(host)
+      status = __status
+      if status == 1
+        unless @event_queue.pop(timeout_ms: 10_000)
+          __abandon
+          raise SocketError, "DNS resolution timed out for #{host}"
+        end
+        status = __status
+      end
+      __release
+      return host if status == 2
+
+      raise SocketError, "DNS resolution failed for #{host}"
+    end
+  end
+end
